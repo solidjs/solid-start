533011f0389eb1c207a0b9e5498f56fa034c9a63
Ryan Carniato <ryansolid@gmail.com>
2021-04-03 00:37:20 -0700

    updates

--- packages/create-solid/templates/app-ts/vite.config.js
+++ packages/create-solid/templates/app-ts/vite.config.js
@@ -1,40 +1,9 @@
 import { defineConfig } from "vite";
-import solid from "vite-plugin-solid";
-import manifest from "rollup-route-manifest";
-import path from "path";
+import solid from "solid-start";
 
 export default defineConfig({
-  resolve: {
-    alias: [{
-      find: "~",
-      replacement: path.join(process.cwd(), "src")
-    }, {
-      find: "@solid-start",
-      replacement: path.join(process.cwd(), "node_modules", "solid-start", "runtime")
-    }]
-  },
-  ssr: {
-    noExternal: ["solid-app-router"]
-  },
   plugins: [
     solid({ ssr: true }),
-    {
-      name: "html-async",
-      enforce: "post",
-      transformIndexHtml(html) {
-        return html.replace('type="module"', 'type="module" async');
-      }
-    },
-    manifest({
-      inline: false,
-      merge: false,
-      publicPath: "/",
-      routes: (file) => {
-        file = file.replace(path.join(__dirname, "src"), "").replace(/\.[tj]sx?$/, "");
-        if (!file.includes("/pages/")) return "*"; // commons
-        return "/" + file.replace("/pages/", "").toLowerCase();
-      }
-    })
   ],
   build: {
     polyfillDynamicImport: false,
---
+++ packages/start/plugin.js
@@ -0,0 +1,45 @@
+import solid from "vite-plugin-solid";
+import manifest from "rollup-route-manifest";
+import path from "path";
+
+export default function StartPlugin(options) {
+  return [
+    solid(options),
+    {
+      name: "solid-start",
+      mode: "pre",
+      config() {
+        return {
+          resolve: {
+            conditions: ['solid'],
+            alias: [{
+              find: "~",
+              replacement: path.join(process.cwd(), "src")
+            }, {
+              find: "@solid-start",
+              replacement: path.join(process.cwd(), "node_modules", "solid-start", "runtime")
+            }]
+          },
+          ssr: {
+            noExternal: ["solid-app-router"]
+          }
+        }
+      }
+    }, {
+      name: "html-async",
+      enforce: "post",
+      transformIndexHtml(html) {
+        return html.replace('type="module"', 'type="module" async');
+      }
+    }, manifest({
+      inline: false,
+      merge: false,
+      publicPath: "/",
+      routes: (file) => {
+        file = file.replace(path.join(__dirname, "src"), "").replace(/\.[tj]sx?$/, "");
+        if (!file.includes("/pages/")) return "*"; // commons
+        return "/" + file.replace("/pages/", "").toLowerCase();
+      }
+    })
+  ]
+}
