305fdda060642bb13ef2f6953f742982619d9e7b
Ryan Carniato <ryansolid@gmail.com>
2022-01-05 23:33:59 -0800

    update template deps

--- packages/start/runtime/entries/devServer.tsx
+++ packages/start/runtime/entries/devServer.tsx
@@ -1,9 +1,9 @@
 import { renderToStream } from "solid-js/web";
 import { render as sRender } from "./server";
 
-export function render({ url, writable }) {
+export function render({ url, writable, context }) {
   renderToStream(
-    sRender({ url, manifest: {} }),
+    sRender({ url, manifest: {}, context }),
   ).pipe(writable);
 }
 
