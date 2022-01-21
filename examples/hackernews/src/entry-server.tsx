5ef6c1fc011159b9f1ca4998d8eeb2a12d9f761c
Nikhil Saraf <nsaraf@purestorage.com>
2022-01-20 23:17:04 -0800

    start refactoring server

--- examples/hackernews/src/entry-server.tsx
+++ examples/hackernews/src/entry-server.tsx
@@ -1,17 +1,26 @@
 import { renderToStream } from "solid-js/web";
 import { StartServer } from "solid-start/components";
 
-export default async function handleRequest({
-  request,
-  manifest,
-  headers,
-  context = {}
-}: {
+interface RequestContext {
   request: Request;
   headers: Response["headers"];
   manifest: Record<string, any>;
   context?: Record<string, any>;
-}) {
+}
+
+function createHandler() {
+  return (ctx: RequestContext) => {
+    const url = new URL(ctx.request.url);
+
+    if (url.pathname.startsWith("/__server_module")) {
+      return new Response("", { status: 404 });
+    }
+
+    return solidPage(ctx);
+  };
+}
+
+async function solidPage({ request, manifest, headers, context = {} }: RequestContext) {
   // streaming
   const { readable, writable } = new TransformStream();
   renderToStream(() => (
@@ -25,3 +34,5 @@ export default async function handleRequest({
     headers
   });
 }
+
+export default createHandler();
