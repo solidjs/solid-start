465a7dcc65561ac3069f7c541e729879df3d2684
Brian Takita <brian.takita@gmail.com>
2022-01-21 11:15:36 -0600

    StartContext: generic types of manifest & context should match manifest & context in entryServer.tsx (#53)
    Fixes https://github.com/solidjs/solid-start/issues/52

--- packages/start/components/StartContext.tsx
+++ packages/start/components/StartContext.tsx
@@ -1,6 +1,9 @@
 import { createContext } from "solid-js";
 
-export const StartContext = createContext<{ manifest?: {}; context?: {} }>({});
+export const StartContext = createContext<{
+  manifest?: Record<string, any>;
+  context?: Record<string, any>;
+}>({});
 
 export function StartProvider(props) {
   return <StartContext.Provider value={props}>{props.children}</StartContext.Provider>;
