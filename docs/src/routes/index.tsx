6ab8309ab16de9583c0a1c3444ced8531638f57d
Zachiah <zachiahsawyer1@gmail.com>
2022-01-31 09:16:43 -0800

    add index route

---
+++ docs/src/routes/index.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Hello world!</h1>
+      <p>
+        Visit{" "}
+        <a href="https://solidjs.com" target="_blank">
+          solidjs.com
+        </a>{" "}
+        to learn how to build Solid apps.
+      </p>
+    </main>
+  );
+}
