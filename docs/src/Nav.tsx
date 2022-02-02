d97b7f2c5857544bb6bfbaf950ad1f653aec4371
Zachiah <zachiahsawyer1@gmail.com>
2022-02-01 17:21:46 -0800

    add a tiny bit of styling to navbar

--- docs/src/Nav.tsx
+++ docs/src/Nav.tsx
@@ -1,14 +1,14 @@
-import { Link } from "solid-app-router";
+import { NavLink } from "solid-app-router";
 
 const Nav = () => {
   return (
     <nav class="flex items-center gap-8 flex-wrap bg-gray-800 p-6">
-      <Link href="/" class="text-white">
+      <NavLink end href="/" class="text-white" activeClass="text-gray-400">
         Home
-      </Link>
-      <Link href="/guides" class="text-white">
+      </NavLink>
+      <NavLink href="/guides" class="text-white" activeClass="text-gray-400">
         Guides
-      </Link>
+      </NavLink>
     </nav>
   );
 };
