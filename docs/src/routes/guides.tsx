eff0aa341b14938f294b5dc00873fbca33b1f5bd
Zachiah <zachiahsawyer1@gmail.com>
2022-02-01 17:11:43 -0800

    fix bug in guides.tsx

--- docs/src/routes/guides.tsx
+++ docs/src/routes/guides.tsx
@@ -4,29 +4,37 @@ import { For } from "solid-js";
 import md from "~/md";
 
 const pathToHumanName = (path: string) =>
+  path // ./guides/page-data.md
+    .split("/") // [".", "guides", "page-data.md"]
+    .at(-1) // "page-data.md"
+    .split(".")[0] // "page-data"
+    .split("-") // ["page", "data"]
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1)) // ["Page", "Data"]
+    .join(" "); // "Page Data"
+
+const pathToLink = (path: string) =>
+  `/guides/${
     path // ./guides/page-data.md
-        .split("/") // [".", "guides", "page-data.md"]
-        .at(-1) // "page-data.md"
-        .split(".")[0] // "page-data"
-        .split("-") // ["page", "data"]
-        .map(word => word.charAt(0).toUpperCase() + word.slice(1)) // ["Page", "Data"]
-        .join(" "); // "Page Data"
-
-
+      .split("/") // [".", "guides", "page-data.md"]
+      .at(-1) // "page-data.md"
+      .split(".")[0] // "page-data"
+  }`;
 
 const Guides = () => {
   return (
-    <md.ul>
-      <For each={Object.keys(modules)}>
-        {name => (
-          <md.li>
-            <md.a href={`/guides/${name}`}>
-                {pathToHumanName(name)}
-            </md.a>
-          </md.li>
-        )}
-      </For>
-    </md.ul>
+    <>
+      <md.ul>
+        <For each={Object.keys(modules)}>
+          {name => (
+            <md.li>
+              <md.a href={pathToLink(name)}>{pathToHumanName(name)}</md.a>
+            </md.li>
+          )}
+        </For>
+      </md.ul>
+
+      <Outlet />
+    </>
   );
 };
 
