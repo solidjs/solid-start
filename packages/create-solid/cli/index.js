6b351632a23506bc778ca0d934428860a7b26c79
Nikhil Saraf <nsaraf@purestorage.com>
2022-02-03 17:49:07 -0800

    fix create-solid to use examples

--- packages/create-solid/cli/index.js
+++ packages/create-solid/cli/index.js
@@ -4,7 +4,35 @@ import { bold, cyan, gray, green, red } from "kleur/colors";
 import path from "path";
 import prompts from "prompts/lib/index";
 import glob from "tiny-glob/sync.js";
+import { viaContentsApi } from "./github.js";
 import { version } from "../package.json";
+import degit from "degit";
+
+const gitIgnore = `
+dist
+worker
+.solid
+.output
+.vercel
+.netlify
+
+# dependencies
+/node_modules
+
+# IDEs and editors
+/.idea
+.project
+.classpath
+*.launch
+.settings/
+
+# Temp
+gitignore
+
+# System Files
+.DS_Store
+Thumbs.db
+`;
 
 const disclaimer = `
 Welcome to the SolidStart setup wizard!
@@ -28,12 +56,47 @@ async function main() {
 
   const target = process.argv[2] || ".";
 
-  const ts_response = await prompts({
-    type: "confirm",
-    name: "value",
-    message: "Use TypeScript?",
-    initial: false
+  let templates = new Set();
+  const templateDirs = await viaContentsApi({
+    directory: "examples",
+    repository: "solid-start",
+    user: "solidjs",
+    ref: "main"
   });
+
+  console.log(
+    templateDirs.forEach(dir => {
+      templates.add(dir.replace("examples/", "").replace(/-ts/, ""));
+    })
+  );
+
+  let templateNames = [...templates];
+  const templateName = (
+    await prompts({
+      type: "select",
+      name: "template",
+      message: "Which template do you want to use?",
+      choices: templateNames.map(name => ({ title: name, value: name })),
+      initial: 1
+    })
+  ).template;
+
+  let ts_response = false;
+
+  if (templateDirs.find(dir => dir.includes(templateName + "-ts"))) {
+    if (templateDirs.find(dir => dir === "examples/" + templateName)) {
+      ts_response = (
+        await prompts({
+          type: "confirm",
+          name: "value",
+          message: "Use TypeScript?",
+          initial: false
+        })
+      ).value;
+    } else {
+      ts_response = true;
+    }
+  }
 
   if (fs.existsSync(target)) {
     if (fs.readdirSync(target).length > 0) {
@@ -52,14 +115,35 @@ async function main() {
     mkdirp(target);
   }
 
-  const cwd = path.join(__dirname, "templates/" + (ts_response.value ? "app-ts" : "app"));
-  const gitignore_contents = fs.readFileSync(path.join(cwd, "gitignore"), "utf-8");
+  let tempTemplate = path.join(target, ".solid-start");
+
+  await new Promise((res, rej) => {
+    const emitter = degit(
+      "solidjs/solid-start/examples/" + templateName + (ts_response ? "-ts" : "") + "#main",
+      {
+        cache: false,
+        force: true,
+        verbose: true
+      }
+    );
+
+    emitter.on("info", info => {
+      console.log(info.message);
+    });
+
+    emitter.clone(path.join(process.cwd(), tempTemplate)).then(() => {
+      res({});
+    });
+  });
+
+  const templateDir = path.join(process.cwd(), tempTemplate);
+  const gitignore_contents = gitIgnore;
   const gitignore = parser.compile(gitignore_contents);
 
-  const files = glob("**/*", { cwd }).filter(gitignore.accepts);
+  const files = glob("**/*", { cwd: templateDir }).filter(gitignore.accepts);
 
   files.forEach(file => {
-    const src = path.join(cwd, file);
+    const src = path.join(templateDir, file);
     const dest = path.join(target, file);
 
     if (fs.statSync(src).isDirectory()) {
@@ -73,10 +157,15 @@ async function main() {
 
   const name = path.basename(path.resolve(target));
 
+  fs.rmSync(path.join(process.cwd(), tempTemplate), {
+    recursive: true,
+    force: true
+  });
+
   const pkg_file = path.join(target, "package.json");
   const pkg_json = fs
     .readFileSync(pkg_file, "utf-8")
-    .replace("~TODO~", name)
+    .replace(/"name": ".+"/, _m => `"name": "${name}"`)
     .replace(/"(.+)": "workspace:.+"/g, (_m, name) => `"${name}": "next"`); // TODO ^${versions[name]}
 
   fs.writeFileSync(pkg_file, pkg_json);
--- packages/create-solid/templates/app/src/entry-client.jsx
+++
@@ -1,4 +0,0 @@
-import { hydrate } from "solid-js/web";
-import { StartClient } from "solid-start/components";
-
-hydrate(() => <StartClient />, document);
