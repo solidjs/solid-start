49fb2a3599de2aacfb9f13b336f74de534520650
Nikhil Saraf <nsaraf@purestorage.com>
2022-02-05 00:30:26 -0800

    fix: docs should use Routes, not Outlet

--- docs/src/root.tsx
+++ docs/src/root.tsx
@@ -32,7 +32,7 @@ export default function Root() {
             </p>{" "}
           </header>
           <Nav />
-          <Outlet />
+          <Routes />
         </MDXProvider>
         <Scripts />
       </body>
