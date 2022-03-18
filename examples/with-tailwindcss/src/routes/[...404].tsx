18e934b9c94f956e3203eba07f8870c436b29b2c
Nikhil Saraf <nsaraf@purestorage.com>
2022-03-17 22:38:23 -0700

    remove DS_Store file

--- examples/with-tailwindcss/src/routes/[...404].tsx
+++ examples/with-tailwindcss/src/routes/[...404].tsx
@@ -1,11 +1,9 @@
-import Counter from "~/components/Counter";
 import { Link } from "solid-app-router";
 
 export default function NotFound() {
   return (
     <main class="text-center mx-auto text-gray-700 p-4">
       <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">Not Found</h1>
-      <Counter />
       <p class="mt-8">
         Visit{" "}
         <Link href="https://solidjs.com" target="_blank" class="text-sky-600 hover:underline">
@@ -16,7 +14,8 @@ export default function NotFound() {
       <p class="my-4">
         <Link href="/" class="text-sky-600 hover:underline">
           Home
-        </Link>{" - "}
+        </Link>
+        {" - "}
         <Link href="/about" class="text-sky-600 hover:underline">
           About Page
         </Link>
