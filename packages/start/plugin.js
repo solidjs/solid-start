083749d25d1938ab7c8dcedf57cb74f32009ce3d
Nikhil Saraf <nsaraf@purestorage.com>
2022-03-27 16:08:28 -0700

    fix client adapter start, fallback to index.html

--- packages/start/plugin.js
+++ packages/start/plugin.js
@@ -21,6 +21,20 @@ function solidStartClientAdpater() {
       var app = connect();
       app.use(compression());
       app.use(config.base, sirv(join(config.root, "dist")));
+      app.use(async (req, res) => {
+        return (await import("fs")).readFile(
+          join(config.root, "dist/index.html"),
+          "utf8",
+          (err, data) => {
+            if (err) {
+              res.statusCode = 500;
+              res.end(`Error: ${err.message}`);
+            } else {
+              res.end(data);
+            }
+          }
+        );
+      });
       http.createServer(app).listen(3000);
       console.log("Listening on http://localhost:3000");
       return;
