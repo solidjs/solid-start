fe6e4405c8d0205f95b1232f10c3290765cd159d
Nikhil Saraf <nsaraf@purestorage.com>
2022-04-02 19:46:29 -0700

    fix crypto in node runtime

--- packages/start/runtime/node-globals.js
+++ packages/start/runtime/node-globals.js
@@ -1,10 +1,11 @@
 import Streams from "stream/web";
-
 import { fetch, Headers, Response, Request } from "undici";
+import crypto from "crypto";
 
 Object.assign(globalThis, Streams, {
   Request,
   Response,
   fetch,
-  Headers
+  Headers,
+  crypto: crypto.webcrypto
 });
