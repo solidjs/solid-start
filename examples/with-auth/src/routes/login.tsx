7a0d35bfca7525b6daaab24a4bcdb6bc86e6addb
Nikhil Saraf <nsaraf@purestorage.com>
2022-05-07 20:30:07 -0700

    fix auth example

--- examples/with-auth/src/routes/login.tsx
+++ examples/with-auth/src/routes/login.tsx
@@ -1,10 +1,10 @@
-import { createForm, FormError } from "solid-start/form";
+import { createAction, FormError } from "solid-start/form";
 import server, { redirect } from "solid-start/server";
 import { createRouteResource } from "solid-start/router";
 import { db } from "~/db";
 import { createUserSession, getUser, login, register } from "~/db/session";
-import { useRouteData, useParams } from "solid-app-router";
-import { createResource, Show } from "solid-js";
+import { useParams } from "solid-app-router";
+import { Show } from "solid-js";
 import { ErrorBoundary } from "solid-start/error-boundary";
 
 function validateUsername(username: unknown) {
@@ -18,66 +18,6 @@ function validatePassword(password: unknown) {
     return `Passwords must be at least 6 characters long`;
   }
 }
-/**
- * This helper function gives us typechecking for our ActionData return
- * statements, while still returning the accurate HTTP status, 400 Bad Request,
- * to the client.
- */
-const loginForm = createForm(
-  server(async (form: FormData) => {
-    const loginType = form.get("loginType");
-    const username = form.get("username");
-    const password = form.get("password");
-    const redirectTo = form.get("redirectTo") || "/";
-    if (
-      typeof loginType !== "string" ||
-      typeof username !== "string" ||
-      typeof password !== "string" ||
-      typeof redirectTo !== "string"
-    ) {
-      throw new FormError(`Form not submitted correctly.`);
-    }
-
-    const fields = { loginType, username, password };
-    const fieldErrors = {
-      username: validateUsername(username),
-      password: validatePassword(password)
-    };
-    if (Object.values(fieldErrors).some(Boolean)) {
-      throw new FormError("Fields invalid", { fieldErrors, fields });
-    }
-
-    switch (loginType) {
-      case "login": {
-        const user = await login({ username, password });
-        if (!user) {
-          throw new FormError(`Username/Password combination is incorrect`, {
-            fields
-          });
-        }
-        return createUserSession(`${user.id}`, redirectTo);
-      }
-      case "register": {
-        const userExists = await db.user.findUnique({ where: { username } });
-        if (userExists) {
-          throw new FormError(`User with username ${username} already exists`, {
-            fields
-          });
-        }
-        const user = await register({ username, password });
-        if (!user) {
-          throw new FormError(`Something went wrong trying to create a new user.`, {
-            fields
-          });
-        }
-        return createUserSession(`${user.id}`, redirectTo);
-      }
-      default: {
-        throw new FormError(`Login type invalid`, { fields });
-      }
-    }
-  })
-);
 
 export default function Login() {
   const [data] = createRouteResource(
@@ -89,6 +29,67 @@ export default function Login() {
     })
   );
 
+  /**
+   * This helper function gives us typechecking for our ActionData return
+   * statements, while still returning the accurate HTTP status, 400 Bad Request,
+   * to the client.
+   */
+  const loginAction = createAction(
+    server(async (form: FormData) => {
+      const loginType = form.get("loginType");
+      const username = form.get("username");
+      const password = form.get("password");
+      const redirectTo = form.get("redirectTo") || "/";
+      if (
+        typeof loginType !== "string" ||
+        typeof username !== "string" ||
+        typeof password !== "string" ||
+        typeof redirectTo !== "string"
+      ) {
+        throw new FormError(`Form not submitted correctly.`);
+      }
+
+      const fields = { loginType, username, password };
+      const fieldErrors = {
+        username: validateUsername(username),
+        password: validatePassword(password)
+      };
+      if (Object.values(fieldErrors).some(Boolean)) {
+        throw new FormError("Fields invalid", { fieldErrors, fields });
+      }
+
+      switch (loginType) {
+        case "login": {
+          const user = await login({ username, password });
+          if (!user) {
+            throw new FormError(`Username/Password combination is incorrect`, {
+              fields
+            });
+          }
+          return createUserSession(`${user.id}`, redirectTo);
+        }
+        case "register": {
+          const userExists = await db.user.findUnique({ where: { username } });
+          if (userExists) {
+            throw new FormError(`User with username ${username} already exists`, {
+              fields
+            });
+          }
+          const user = await register({ username, password });
+          if (!user) {
+            throw new FormError(`Something went wrong trying to create a new user.`, {
+              fields
+            });
+          }
+          return createUserSession(`${user.id}`, redirectTo);
+        }
+        default: {
+          throw new FormError(`Login type invalid`, { fields });
+        }
+      }
+    })
+  );
+
   const params = useParams();
   return (
     <div className="p-4">
@@ -96,7 +97,7 @@ export default function Login() {
         <main class="p-6 mx-auto w-[fit-content] space-y-4 rounded-lg bg-gray-100">
           <h1 class="font-bold text-xl">Login</h1>
           <ErrorBoundary fallback={() => <div>Error</div>}>
-            <loginForm.Form key="login" method="post" class="flex flex-col space-y-2">
+            <loginAction.Form key="login" method="post" class="flex flex-col space-y-2">
               <input type="hidden" name="redirectTo" value={params.redirectTo ?? "/"} />
               <fieldset class="flex flex-row">
                 <legend className="sr-only">Login or Register?</legend>
@@ -114,9 +115,9 @@ export default function Login() {
                   placeholder="kody"
                   class="border-gray-700 border-2 ml-2 rounded-md px-2"
                 />
-                <Show when={loginForm.submission("login")?.error?.fieldErrors?.username}>
+                <Show when={loginAction.submission("login")?.error?.fieldErrors?.username}>
                   <p class="text-red-400" role="alert">
-                    {loginForm.submission("login")?.error.fieldErrors.username}
+                    {loginAction.submission("login")?.error.fieldErrors.username}
                   </p>
                 </Show>
               </div>
@@ -128,15 +129,15 @@ export default function Login() {
                   placeholder="twixrox"
                   class="border-gray-700 border-2 ml-2 rounded-md px-2"
                 />
-                <Show when={loginForm.submission("login")?.error?.fieldErrors?.password}>
+                <Show when={loginAction.submission("login")?.error?.fieldErrors?.password}>
                   <p class="text-red-400" role="alert">
-                    {loginForm.submission("login")?.error.fieldErrors.password}
+                    {loginAction.submission("login")?.error.fieldErrors.password}
                   </p>
                 </Show>
               </div>
-              <Show when={loginForm.submission("login")?.error}>
+              <Show when={loginAction.submission("login")?.error}>
                 <p class="text-red-400" role="alert" id="error-message">
-                  {loginForm.submissions()["login"]?.error.message}
+                  {loginAction.submissions()["login"]?.error.message}
                 </p>
               </Show>
               <button
@@ -145,7 +146,7 @@ export default function Login() {
               >
                 {data() ? "Login" : ""}
               </button>
-            </loginForm.Form>
+            </loginAction.Form>
           </ErrorBoundary>
         </main>
       </div>
