70667ffa0f27e6f0d6046bb4463e64947802235b
Nikhil Saraf <nsaraf@purestorage.com>
2022-05-12 00:39:36 -0700

    remove unnecessary examples for now

--- examples/with-prisma/src/db/index.tsx
+++
@@ -1,6 +0,0 @@
-import P from "@prisma/client";
-const { PrismaClient } = P;
-
-export const db = new PrismaClient();
-
-db.$connect();
--- examples/with-prisma/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import solid from "solid-start";
-import windicss from "vite-plugin-windicss";
-
-export default defineConfig({
-  plugins: [windicss(), solid()]
-});
