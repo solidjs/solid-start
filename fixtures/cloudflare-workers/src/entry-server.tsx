ed87f363033697fce5628003a11d668e0e3b7bfb
Nikhil Saraf <nsaraf@purestorage.com>
2022-05-12 01:00:58 -0700

    remove inlineServerFunctions from entry server (make it part of default)

--- fixtures/cloudflare-workers/src/entry-server.tsx
+++ fixtures/cloudflare-workers/src/entry-server.tsx
@@ -1,7 +1,3 @@
 import { StartServer, createHandler, renderAsync } from "solid-start/entry-server";
-import { inlineServerModules } from "solid-start/server";
 
-export default createHandler(
-  inlineServerModules,
-  renderAsync((context) => <StartServer context={context} />)
-);
+export default createHandler(renderAsync(context => <StartServer context={context} />));
--- fixtures/kitchen-sink/src/entry-server.tsx
+++ fixtures/kitchen-sink/src/entry-server.tsx
@@ -1,9 +1,3 @@
 import { StartServer, createHandler, renderAsync } from "solid-start/entry-server";
-import { inlineServerModules } from "solid-start/server";
-import { apiRoutes } from "solid-start/api";
 
-export default createHandler(
-  apiRoutes,
-  inlineServerModules,
-  renderAsync(context => <StartServer context={context} />)
-);
+export default createHandler(renderAsync(context => <StartServer context={context} />));
