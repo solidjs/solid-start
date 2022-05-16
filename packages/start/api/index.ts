b46c5d32d9ba0328e6d8eb41947eb5f07ef1a0c2
Ryan Carniato <ryansolid@gmail.com>
2022-05-15 22:26:36 -0700

    small fixes

--- packages/start/api/index.ts
+++ packages/start/api/index.ts
@@ -1,9 +1,3 @@
-import type { RequestContext } from "../server/types";
-
-// the line below will be replaced by the compiler with a configuration of routes
-// based on the files in src/routes
-// @ts-ignore
-
 // the line below will be replaced by the compiler with a configuration of routes
 // based on the files in src/routes
 // we have to keep this var so that we can find it even if vite precompiles the code
@@ -19,4 +13,3 @@ export function getApiHandler(url: URL, method: string) {
     }
   }
 }
-
