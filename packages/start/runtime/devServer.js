a9289ed3ad252c3198cbb75d65b480cae4ee942b
Nikhil Saraf <nsaraf98@gmail.com>
2022-06-21 22:05:21 -0700

    add test for css

--- packages/start/runtime/devServer.js
+++ packages/start/runtime/devServer.js
@@ -5,6 +5,10 @@ import { Headers } from "undici";
 import { createRequest } from "./fetch.js";
 import "./node-globals.js";
 
+// Vite doesn't expose this so we just copy the list for now
+// https://github.com/vitejs/vite/blob/3edd1af56e980aef56641a5a51cf2932bb580d41/packages/vite/src/node/plugins/css.ts#L96
+const style_pattern = /\.(css|less|sass|scss|styl|stylus|pcss|postcss)$/;
+
 export function createDevHandler(viteServer) {
   return async (req, res) => {
     try {
@@ -12,7 +16,39 @@ export function createDevHandler(viteServer) {
 
       console.log(req.method, req.url);
 
-      const entry = (await viteServer.ssrLoadModule(path.resolve("./src/entry-server"))).default;
+      let moduleURL = path.resolve("./src/entry-server");
+      const entry = (await viteServer.ssrLoadModule(moduleURL)).default;
+
+      const node = await viteServer.moduleGraph.getModuleByUrl(moduleURL);
+
+      if (!node) throw new Error(`Could not find node for ${moduleURL}`);
+      console.log(node);
+
+      const deps = new Set();
+      await find_deps(viteServer, node, deps);
+
+      console.log(deps);
+
+      /** @type {Record<string, string>} */
+      const styles = {};
+
+      for (const dep of deps) {
+        const parsed = new URL(dep.url, "http://localhost/");
+        const query = parsed.searchParams;
+
+        if (style_pattern.test(dep.file)) {
+          try {
+            const mod = await viteServer.ssrLoadModule(dep.url, { fixStacktrace: false });
+            styles[dep.url] = mod.default;
+          } catch {
+            // this can happen with dynamically imported modules, I think
+            // because the Vite module graph doesn't distinguish between
+            // static and dynamic imports? TODO investigate, submit fix
+          }
+        }
+      }
+
+      console.log(styles);
 
       const webRes = await entry({
         request: createRequest(req),
@@ -40,4 +76,38 @@ export function createDevHandler(viteServer) {
       res.end(e.stack);
     }
   };
+}
+
+async function find_deps(vite, node, deps) {
+  // since `ssrTransformResult.deps` contains URLs instead of `ModuleNode`s, this process is asynchronous.
+  // instead of using `await`, we resolve all branches in parallel.
+  /** @type {Promise<void>[]} */
+  const branches = [];
+
+  /** @param {import('vite').ModuleNode} node */
+  async function add(node) {
+    if (!deps.has(node)) {
+      deps.add(node);
+      await find_deps(vite, node, deps);
+    }
+  }
+
+  /** @param {string} url */
+  async function add_by_url(url) {
+    const node = await vite.moduleGraph.getModuleByUrl(url);
+
+    if (node) {
+      await add(node);
+    }
+  }
+
+  if (node.ssrTransformResult) {
+    if (node.ssrTransformResult.deps) {
+      node.ssrTransformResult.deps.forEach(url => branches.push(add_by_url(url)));
+    }
+  } else {
+    node.importedModules.forEach(node => branches.push(add(node)));
+  }
+
+  await Promise.all(branches);
 }
---
+++ test/css-test.ts
@@ -0,0 +1,112 @@
+import { test, expect } from "@playwright/test";
+
+import { createAppFixture, createFixture, css, js } from "./helpers/create-fixture.js";
+import type { AppFixture, Fixture } from "./helpers/create-fixture.js";
+import { PlaywrightFixture, prettyHtml, selectHtml } from "./helpers/playwright-fixture.js";
+
+test.describe("css", () => {
+  let fixture: Fixture;
+  let appFixture: AppFixture;
+
+  test.beforeAll(async () => {
+    fixture = await createFixture({
+      files: {
+        "src/root.css": css`
+          body {
+            margin: 0;
+          }
+        `,
+        "src/root.tsx": js`
+          import { Links, Meta, Routes, Scripts } from "solid-start/root";
+          import { Suspense } from "solid-js";
+          import "./root.css";
+
+          export default function Root() {
+            return (
+              <html lang="en">
+                <head>
+                  <meta charset="utf-8" />
+                  <meta name="viewport" content="width=device-width, initial-scale=1" />
+                  <Meta />
+                  <Links />
+                </head>
+                <body>
+                  <div id="content">
+                    <h1>Root</h1>
+                    <Routes />
+                  </div>
+                  <Scripts />
+                </body>
+              </html>
+            );
+          }
+        `,
+        "src/routes/index.css": css`
+          body {
+            background-color: red;
+          }
+        `,
+        "src/routes/index.tsx": js`
+          import "./index.css";
+          export default function Index() {
+            return <h1>Index</h1>;
+          }
+        `,
+        "src/routes/other.css": css`
+          body {
+            background-color: blue;
+          }
+        `,
+        "src/routes/other.tsx": js`
+          import "./other.css";
+          export default function Other() {
+            return <h1>Other</h1>;
+          }
+        `
+      }
+    });
+
+    appFixture = await createAppFixture(fixture);
+  });
+
+  test.afterAll(async () => {
+    await appFixture.close();
+  });
+
+  let logs: string[] = [];
+
+  test.beforeEach(({ page }) => {
+    page.on("console", msg => {
+      logs.push(msg.text());
+    });
+  });
+
+  test("server renders matching routes", async () => {
+    let res = await fixture.requestDocument("/");
+    expect(res.status).toBe(200);
+    expect(res.headers.get("Content-Type")).toBe("text/html");
+    // expect(selectHtml(await res.text(), "#content")).toBe(
+    //   prettyHtml(`
+    // <div id="content">
+    //   <h1>Root</h1>
+    //   <!--#-->
+    //   <h2 data-hk="0-0-0-0-0-0-0-0-1-0-0-0-0-0">Index</h2>
+    //   <!--/-->
+    // </div>`)
+    // );
+  });
+
+  // test("hydrates", async ({ page }) => {
+  //   let app = new PlaywrightFixture(appFixture, page);
+  //   await app.goto("/");
+  //   expect(await app.getHtml("#content")).toBe(
+  //     prettyHtml(`
+  //   <div id="content">
+  //     <h1>Root</h1>
+  //     <!--#-->
+  //     <h2 data-hk="0-0-0-0-0-0-0-0-1-0-0-0-0-0">Index</h2>
+  //     <!--/-->
+  //   </div>`)
+  //   );
+  // });
+});
