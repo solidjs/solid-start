803e112d79aef217cc3373bc47c4b1855b547676
Nikhil Saraf <nsaraf98@gmail.com>
2022-07-03 00:56:51 -0700

    fix examples for server API change

--- examples/with-mdx/src/entry-server.tsx
+++ examples/with-mdx/src/entry-server.tsx
@@ -1,3 +1,3 @@
 import { StartServer, createHandler, renderAsync } from "solid-start/entry-server";
 
-export default createHandler(renderAsync(context => <StartServer context={context} />));
+export default createHandler(renderAsync(event => <StartServer event={event} />));
