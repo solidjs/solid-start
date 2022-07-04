a7b9ab94a4b5a37106f41d11f4c53e96b99359ba
Nikhil Saraf <nsaraf98@gmail.com>
2022-07-03 19:09:31 -0700

    fix do-ws example

--- examples/durable-objects-websocket/vite.config.ts
+++ examples/durable-objects-websocket/vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 import solid from "solid-start";
-import worker from "solid-start-cloudflare-workers";
+import cloudflareWorkers from "solid-start-cloudflare-workers";
 
 export default defineConfig({
   plugins: [
     solid({
-      adapter: worker({
-        reexport: ["WebSocketDurableObject"]
+      adapter: cloudflareWorkers({
+        durableObjects: ["WebSocketDurableObject"]
       })
     })
   ]
