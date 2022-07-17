436b9d90b76f771491f5cd184b9339157aceae81
Benjamin Bender <bb@codepoet.de>
2022-07-17 17:12:39 +0200

    fix: correctly load env in vite-conf. Removes global-setting for vitest as it's not recommended

--- examples/with-vitest/vite.config.ts
+++ examples/with-vitest/vite.config.ts
@@ -1,29 +1,34 @@
 /// <reference types="vitest" />
 
 import solid from "solid-start";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-  test: {
-    globals: true,
-    environment: "jsdom",
-    transformMode: {
-      web: [/\.[tj]sx?$/]
+export default defineConfig(({ mode }) => {
+	// Load env file based on `mode` in the current working directory.
+	// Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+	const env = loadEnv(mode, process.cwd());
+
+  return {
+    test: {
+      environment: "jsdom",
+      transformMode: {
+        web: [/\.[tj]sx?$/]
+      },
+      setupFiles: "./vitest.setup.ts",
+      // solid needs to be inline to work around
+      // a resolution issue in vitest:
+      deps: {
+        inline: [/solid-js/]
+      }
+      // if you have few tests, try commenting one
+      // or both out to improve performance:
+      // threads: false,
+      // isolate: false,
     },
-    setupFiles: "./vitest.setup.ts",
-    // solid needs to be inline to work around
-    // a resolution issue in vitest:
-    deps: {
-      inline: [/solid-js/]
-    }
-    // if you have few tests, try commenting one
-    // or both out to improve performance:
-    // threads: false,
-    // isolate: false,
-  },
-  plugins: [solid()],
-  
-  resolve: {
-		conditions: process.env["VITEST"] ? ["browser"] : [],
-	},
+    plugins: [solid()],
+    
+    resolve: {
+      conditions: env["VITEST"] ? ["browser"] : [],
+    },
+  }
 });
