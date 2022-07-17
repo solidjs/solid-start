43197ed918b2b40175cb957303cc8ec64e92e4ba
Benjamin Bender <bb@codepoet.de>
2022-07-17 17:13:34 +0200

    fix: correctly load env in vite-conf.

--- examples/with-vitest/vite.config.ts
+++ examples/with-vitest/vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig, loadEnv } from "vite";
 export default defineConfig(({ mode }) => {
 	// Load env file based on `mode` in the current working directory.
 	// Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-	const env = loadEnv(mode, process.cwd());
+	const env = loadEnv(mode, process.cwd(), '');
 
   return {
     test: {
