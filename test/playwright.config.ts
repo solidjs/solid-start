9a0ac8918c2691d125cd74153815924cfece2efa
Nikhil Saraf <nsaraf98@gmail.com>
2022-07-19 17:07:24 -0700

    tests

--- test/playwright.config.ts
+++ test/playwright.config.ts
@@ -12,7 +12,7 @@ const config: PlaywrightTestConfig = {
   },
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 3 : undefined,
   reporter: process.env.CI ? "github" : [["html", { open: "never" }]],
   use: { actionTimeout: 0 },
   projects: [
