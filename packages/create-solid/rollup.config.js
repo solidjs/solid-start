9f2004e2a8e668829e17c9a01c8960b58916d40d
Ryan Carniato <ryansolid@gmail.com>
2022-07-25 16:10:04 -0700

    fix create-solid build

--- packages/create-solid/rollup.config.js
+++ packages/create-solid/rollup.config.js
@@ -1,6 +1,6 @@
-import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 
 export default {
   input: "cli/index.js",
@@ -8,7 +8,8 @@ export default {
     file: "bin",
     format: "cjs",
     banner: "#!/usr/bin/env node\nglobal.navigator={}",
-    interop: false
+    interop: false,
+    inlineDynamicImports: true
   },
   plugins: [nodeResolve(), commonjs(), json()],
   external: require("module").builtinModules
