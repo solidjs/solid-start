2ac56bb32d8fdb5b5e94d811e9a16ac64f1b1d07
Nikhil Saraf <nsaraf98@gmail.com>
2022-07-26 02:57:02 -0700

    clean mount code

--- packages/start/entry-client/mount.tsx
+++ packages/start/entry-client/mount.tsx
@@ -25,36 +25,34 @@ export default function mount(code?: () => JSX.Element, element?: Document) {
   if (import.meta.env.START_ISLANDS) {
     mountRouter();
 
-    if (import.meta.env.START_ISLANDS) {
-      async function mountIsland(el: HTMLElement) {
-        let Component = window._$HY.islandMap[el.dataset.island];
-        if (!Component) {
-          await import(/* @vite-ignore */ el.dataset.component);
-          Component = window._$HY.islandMap[el.dataset.island];
-        }
-
-        DEBUG(
-          "hydrating island",
-          el.dataset.island,
-          el.dataset.hk.slice(0, el.dataset.hk.length - 1) + `2-`,
-          el
-        );
-
-        hydrate(() => createComponent(Component, JSON.parse(el.dataset.props)), el, {
-          renderId: el.dataset.hk.slice(0, el.dataset.hk.length - 1) + `2-`
-        });
+    async function mountIsland(el: HTMLElement) {
+      let Component = window._$HY.islandMap[el.dataset.island];
+      if (!Component) {
+        await import(/* @vite-ignore */ el.dataset.component);
+        Component = window._$HY.islandMap[el.dataset.island];
       }
 
-      window._$HY.hydrateIslands = () => {
-        document.querySelectorAll("solid-island").forEach((el: HTMLElement) => {
-          if (el.dataset.when === "idle") {
-            requestIdleCallback(() => mountIsland(el));
-          } else {
-            mountIsland(el as HTMLElement);
-          }
-        });
-      };
+      DEBUG(
+        "hydrating island",
+        el.dataset.island,
+        el.dataset.hk.slice(0, el.dataset.hk.length - 1) + `2-`,
+        el
+      );
+
+      hydrate(() => createComponent(Component, JSON.parse(el.dataset.props)), el, {
+        renderId: el.dataset.hk.slice(0, el.dataset.hk.length - 1) + `2-`
+      });
     }
+
+    window._$HY.hydrateIslands = () => {
+      document.querySelectorAll("solid-island").forEach((el: HTMLElement) => {
+        if (el.dataset.when === "idle") {
+          requestIdleCallback(() => mountIsland(el));
+        } else {
+          mountIsland(el as HTMLElement);
+        }
+      });
+    };
 
     window._$HY.hydrateIslands();
 
