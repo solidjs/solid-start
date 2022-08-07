abf57ea054d9c789d77b8eed95cfd00b36c244d7
Nikhil Saraf <nsaraf98@gmail.com>
2022-08-06 19:35:35 -0700

    fix with-tailwindcss example #228

--- examples/with-tailwindcss/vite.config.ts
+++ examples/with-tailwindcss/vite.config.ts
@@ -2,5 +2,9 @@ import solid from "solid-start/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [solid()]
+  plugins: [
+    solid({
+      ssr: false
+    })
+  ]
 });
