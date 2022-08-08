53703d30bb8ce40548edb545dd8e1f3e267889d6
Sam C. Finan <samcfinan@gmail.com>
2022-08-08 09:56:31 -0400

    Set by default, append if prop set

--- packages/start/server/components/HttpHeader.tsx
+++ packages/start/server/components/HttpHeader.tsx
@@ -2,11 +2,15 @@ import { onCleanup, useContext } from "solid-js";
 import { isServer } from "solid-js/web";
 import { ServerContext } from "../ServerContext";
 
-export function HttpHeader(props: { name: string; value: string }) {
+export function HttpHeader(props: { name: string; value: string; append?: boolean }) {
   const pageContext = useContext(ServerContext);
 
   if (isServer) {
-    pageContext.responseHeaders.append(props.name, props.value);
+    if (props.append) {
+      pageContext.responseHeaders.append(props.name, props.value);
+    } else {
+      pageContext.responseHeaders.set(props.name, props.value);
+    }
   }
 
   onCleanup(() => {
