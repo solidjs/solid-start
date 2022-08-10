23bb515953c55eaaac93b5c6a1c02750825bdd77
Nikhil Saraf <nsaraf98@gmail.com>
2022-08-10 09:42:16 -0700

    Update index.ts

--- packages/start/session/index.ts
+++ packages/start/session/index.ts
@@ -1,9 +1,8 @@
+// All credits to Remix team:
+// https://github.com/remix-run/remix/blob/main/packages/remix-server-runtime
 import { createCookieFactory } from "./cookies";
 import { createCookieSessionStorageFactory } from "./cookieStorage";
 import { createSessionStorageFactory } from "./sessions";
-// TODO: Once node v16 is available on AWS we should use these instead of the
-// global `sign` and `unsign` functions.
-//import { sign, unsign } from "./cookieSigning";
 import "./cookieSigning";
 import { sign, unsign } from "./cookieSigning";
 import { createMemorySessionStorageFactory } from "./memoryStorage";
