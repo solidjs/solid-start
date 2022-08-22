2fb580a89797476522a453cf327cac52a66f86cc
Nikhil Saraf <nsaraf98@gmail.com>
2022-08-21 23:29:42 -0700

    remove docs tailwind config

--- tailwind.config.cjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./docs/**/*.{js,jsx,ts,tsx}", "./root.docs.tsx"],
-
-  theme: {
-    extend: {}
-  },
-  plugins: [require("@tailwindcss/typography")]
-};
