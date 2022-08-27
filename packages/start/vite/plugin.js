3507d57c934e5a7f6f7583933946e6c4373af79a
Nikhil Saraf <nsaraf98@gmail.com>
2022-08-26 23:02:42 -0700

    remove test config

--- packages/start/vite/plugin.js
+++ packages/start/vite/plugin.js
@@ -423,23 +423,6 @@ function solidStartConfig(options) {
       DEBUG(options);
 
       return {
-        test: {
-          environment: "jsdom",
-          transformMode: {
-            web: [/\.[tj]sx?$/]
-          },
-          globals: true,
-          setupFiles: "node_modules/solid-start/vitest.setup.ts",
-          // solid needs to be inline to work around
-          // a resolution issue in vitest:
-          // deps: {
-          //   inline: [/solid-js/]
-          // }
-          // if you have few tests, try commenting one
-          // or both out to improve performance:
-          threads: false,
-          isolate: false
-        },
         resolve: {
           conditions: options.env["VITEST"] ? ["browser", "solid"] : ["solid"],
           alias: {
