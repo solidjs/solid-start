b6a00f363ff5532fb2281fca503693d2d35efddb
Nikhil Saraf <nsaraf98@gmail.com>
2022-09-25 18:20:27 -0700

    miniflare dev environment for cloudlfare pages

--- packages/start-cloudflare-pages/index.js
+++ packages/start-cloudflare-pages/index.js
@@ -6,10 +6,105 @@ import { copyFileSync } from "fs";
 import { dirname, join } from "path";
 import { rollup } from "rollup";
 import { fileURLToPath } from "url";
-
-export default function () {
+import { Miniflare } from "miniflare";
+import { createServer } from "./dev-server.js";
+export default function (miniflareOptions) {
   return {
     name: "cloudflare-pages",
+    async dev(options, vite, dev) {
+      const mf = new Miniflare({
+        script: `
+        export default {
+          fetch: async (request, env) => {
+            return await serve(request, env, globalThis);
+          }
+        }
+
+        export const WebSocketDurableObject = WebSocketDurableObject1;
+      `,
+        globals: {
+          WebSocketDurableObject1: class DO {
+            state;
+            env;
+            promise;
+            constructor(state, env) {
+              this.state = state;
+              this.env = env;
+              this.promise = this.createProxy(state, env);
+            }
+
+            async createProxy(state, env) {
+              const { WebSocketDurableObject } = await vite.ssrLoadModule("~start/entry-server");
+              return new WebSocketDurableObject(state, env);
+            }
+
+            async fetch(request) {
+              console.log("DURABLE_OBJECT", request.url);
+
+              try {
+                let dObject = await this.promise;
+                return await dObject.fetch(request);
+              } catch (e) {
+                console.log("error", e);
+              }
+            }
+          },
+          serve: async (req, e, g) => {
+            const {
+              Request,
+              Response,
+              fetch,
+              crypto,
+              Headers,
+              ReadableStream,
+              WritableStream,
+              WebSocketPair,
+              TransformStream
+            } = g;
+            Object.assign(globalThis, {
+              Request,
+              Response,
+              fetch,
+              crypto,
+              Headers,
+              ReadableStream,
+              WritableStream,
+              TransformStream,
+              WebSocketPair
+            });
+
+            console.log(
+              "🔥",
+              req.headers.get("Upgrade") === "websocket" ? "WEBSOCKET" : req.method,
+              req.url
+            );
+
+            if (req.headers.get("Upgrade") === "websocket") {
+              const url = new URL(req.url);
+              console.log(url.search);
+              const durableObjectId = e.DO_WEBSOCKET.idFromName(url.pathname + url.search);
+              const durableObjectStub = e.DO_WEBSOCKET.get(durableObjectId);
+              const response = await durableObjectStub.fetch(req);
+              return response;
+            }
+
+            try {
+              return await dev.fetch(req, e);
+            } catch (e) {
+              console.log("error", e);
+              return new Response(e.toString(), { status: 500 });
+            }
+          }
+        },
+        modules: true,
+        kvPersist: true,
+        ...miniflareOptions
+      });
+
+      console.log("🔥", "starting miniflare");
+
+      return await createServer(vite, mf, {});
+    },
     start(config, { port }) {
       process.env.PORT = port;
       const proc = spawn("node", [
