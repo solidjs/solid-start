45586e6cca6f7006151ed85e112c4e5d77ed5954
Nikhil Saraf <nsaraf98@gmail.com>
2022-10-08 22:45:22 -0700

    fix tooltip

--- components/tooltip.tsx
+++ components/tooltip.tsx
@@ -1,16 +1,22 @@
 import { createEffect } from "solid-js";
-import tippy from "tippy.js";
+import { isServer } from "solid-js/web";
+let tippy;
+if (!isServer) {
+  tippy = await import("tippy.js");
+}
 
 export default function Tooltip(props) {
   createEffect(() => {
     console.log("tippy");
-    tippy(`[data-template="${props.id}"]`, {
-      content() {
-        const template = document.getElementById(props.id);
-        return template.innerHTML;
-      },
-      allowHTML: true
-    });
+    if (!isServer) {
+      tippy(`[data-template="${props.id}"]`, {
+        content() {
+          const template = document.getElementById(props.id);
+          return template.innerHTML;
+        },
+        allowHTML: true
+      });
+    }
   });
   return (
     <span class={`data-lsp`} data-template={props.id}>
