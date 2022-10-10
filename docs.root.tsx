bc796fc8171a82e38df40bd2b75bd894ea14492d
Nikhil Saraf <nsaraf98@gmail.com>
2022-10-10 16:13:26 -0700

    update favicon

--- docs.root.tsx
+++ docs.root.tsx
@@ -6,6 +6,7 @@ import {
   FileRoutes,
   Head,
   Html,
+  Link,
   Meta,
   Routes,
   Scripts,
@@ -166,6 +167,7 @@ export default function Root() {
         <Title>SolidStart</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Link rel="icon" href="/favicon.ico" />
         <Stylesheet href="https://cdn.jsdelivr.net/npm/@docsearch/css@3" />
       </Head>
       <Body>
