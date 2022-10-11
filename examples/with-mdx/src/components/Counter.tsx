dc816fff92e04af236cf4232eb0b2b5510d5a3c8
Marko <marko@pepega.club>
2022-10-11 19:07:35 +0200

    Fix reactivity usage in templates (#301)

--- examples/with-mdx/src/components/Counter.tsx
+++ examples/with-mdx/src/components/Counter.tsx
@@ -5,7 +5,7 @@ export default function Counter() {
   const [count, setCount] = createSignal(0);
   return (
     <button class="increment" onClick={() => setCount(count() + 1)}>
-      Clicks: {count}
+      Clicks: {count()}
     </button>
   );
 }
