22268e0e22323f6f851de812191495d61c5d5c24
Muhammad Zaki <muhammad.zaki93@ui.ac.id>
2022-10-25 15:05:04 +0700

    example(movies): fix search param feature

--- examples/movies/src/routes/search/SearchBox.tsx
+++ examples/movies/src/routes/search/SearchBox.tsx
@@ -1,22 +1,21 @@
 import { debounce } from "@solid-primitives/scheduled";
-import { createEffect, createSignal } from "solid-js";
+import { createSignal } from "solid-js";
 import { useLocation, useNavigate } from "solid-start";
 import styles from "./SearchBox.module.scss";
 
 export default function Input(props) {
-  const [value, setValue] = createSignal("");
+  const [value, setValue] = createSignal(props.value || "");
   const navigate = useNavigate();
   const location = useLocation();
 
-  createEffect(() => {
-    if (value().length) {
-      navigate(`${location.pathname}?q=${value()}`);
+  const update = (newValue: string) => {
+    if (newValue.length && newValue !== value()) {
+      setValue(newValue);
+      navigate(`${location.pathname}?q=${newValue}`);
     }
-  });
+  };
 
-  const debouncedUpdate = debounce((value: string) => {
-    setValue(value);
-  }, 500);
+  const debouncedUpdate = debounce(update, 500);
 
   const goBack = () => {
     // !TODO
@@ -25,20 +24,27 @@ export default function Input(props) {
   return (
     <>
       <div class={styles.form}>
-        <form autocomplete="off">
-          <label class="visuallyhidden" for="search">
+        <form autocomplete="off" onSubmit={e => e.preventDefault()}>
+          <label class="visuallyhidden" for="q">
             Search
           </label>
 
           <div class={styles.field}>
             <input
-              id="search"
-              name="search"
+              id="q"
+              name="q"
               type="text"
               placeholder="Search for a movie, tv show or person..."
               // keyup="goToRoute"
               // blur="unFocus"
               onInput={e => debouncedUpdate(e.currentTarget.value)}
+              onKeyUp={e => {
+                e.preventDefault();
+                if (e.key === "Enter") {
+                  debouncedUpdate.clear();
+                  update(e.currentTarget.value);
+                }
+              }}
               value={value()}
             />
             <button v-if="showButton" type="button" aria-label="Close" onClick={goBack}>
