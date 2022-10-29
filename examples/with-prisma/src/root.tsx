1c12f978ba320f1572dcc62d313377e149dd2ba5
Nikhil Saraf <nsaraf98@gmail.com>
2022-10-29 00:19:38 -0700

    prisma example

---
+++ examples/with-prisma/src/root.tsx
@@ -0,0 +1,36 @@
+// @refresh reload
+import { Suspense } from "solid-js";
+import {
+  Body,
+  ErrorBoundary,
+  FileRoutes,
+  Head,
+  Html,
+  Meta,
+  Routes,
+  Scripts,
+  Title
+} from "solid-start";
+import "./root.css";
+
+export default function Root() {
+  return (
+    <Html lang="en">
+      <Head>
+        <Title>SolidStart - With Auth</Title>
+        <Meta charset="utf-8" />
+        <Meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Body>
+        <ErrorBoundary>
+          <Suspense fallback={<div>Loading</div>}>
+            <Routes>
+              <FileRoutes />
+            </Routes>
+          </Suspense>
+        </ErrorBoundary>
+        <Scripts />
+      </Body>
+    </Html>
+  );
+}
