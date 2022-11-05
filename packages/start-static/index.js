002b82ddfd9746cd483a747b13b2fc91a8449a60
Nikhil Saraf <nsaraf98@gmail.com>
2022-11-05 14:33:39 -0700

    fix static adapter to build the entry for node

--- packages/start-static/index.js
+++ packages/start-static/index.js
@@ -1,8 +1,11 @@
 /// <reference types="node" />
-
+import common from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import nodeResolve from "@rollup/plugin-node-resolve";
 import { spawn } from "child_process";
 import { copyFileSync } from "fs";
 import { dirname, join, resolve } from "path";
+import { rollup } from "rollup";
 import renderStatic from "solid-ssr/static";
 import { fileURLToPath } from "url";
 
@@ -13,14 +16,14 @@ export default function () {
     name: "static",
     start(config, { port }) {
       process.env.PORT = port;
-      const proc = spawn("npx", ["serve", "./dist/public", "--port", `${process.env.PORT}`]);
+      const proc = spawn("npx", ["serve", "./dist/public"]);
       proc.stdout.pipe(process.stdout);
       proc.stderr.pipe(process.stderr);
 
       return `http://localhost:${process.env.PORT}`;
     },
     async build(config, builder) {
-      const appRoot = config.solidOptions.appRoot;
+      const ssrExternal = config?.ssr?.external || [];
       await builder.client(join(config.root, "dist", "public"));
       await builder.server(join(config.root, ".solid", "server"));
       copyFileSync(
@@ -30,21 +33,41 @@ export default function () {
       const pathToServer = join(config.root, ".solid", "server", "server.js");
       copyFileSync(join(__dirname, "entry.js"), pathToServer);
       const pathToDist = resolve(config.root, "dist", "public");
-      const pageRoot = join(config.root, appRoot, config.solidOptions.routesDir);
-      console.log(pageRoot);
+
+      builder.debug(`bundling server with rollup`);
+
+      const bundle = await rollup({
+        input: join(config.root, ".solid", "server", "server.js"),
+        plugins: [
+          json(),
+          nodeResolve({
+            preferBuiltins: true,
+            exportConditions: ["node", "solid"]
+          }),
+          common()
+        ],
+        external: ["undici", "stream/web", ...ssrExternal]
+      });
+      // or write the bundle to disk
+      await bundle.write({ format: "esm", dir: join(config.root, "dist") });
+
+      // closes the bundle
+      await bundle.close();
+
+      builder.debug(`bundling server done`);
 
       await config.solidOptions.router.init();
       const routes = [
         ...config.solidOptions.router
           .getFlattenedPageRoutes()
           .map(a => a.path)
-          .filter(a => a.includes(":") || a.includes("/")),
+          .filter(a => (a.includes(":") || a.includes("/")) && !a.includes("*")),
         "/404",
         ...(config.solidOptions.prerenderRoutes || [])
       ];
       renderStatic(
         routes.map(url => ({
-          entry: pathToServer,
+          entry: join(config.root, "dist", "server.js"),
           output: join(
             pathToDist,
             url.endsWith("/") ? `${url.slice(1)}index.html` : `${url.slice(1)}.html`
