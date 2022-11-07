e087c0b6f7912c11a69a25eb36e54501c3bdd2c0
Nikhil Saraf <nsaraf98@gmail.com>
2022-11-07 00:18:11 -0800

    remove nprogress

--- examples/movies/src/entry-client.tsx
+++ examples/movies/src/entry-client.tsx
@@ -1,17 +1,3 @@
-import Nprogress from "nprogress";
-
 import { mount, StartClient } from "solid-start/entry-client";
 
 mount(() => <StartClient />, document);
-
-window.router.router.addEventListener("navigation-start", e => {
-  Nprogress.start();
-});
-
-window.router.router.addEventListener("navigation-end", e => {
-  Nprogress.done();
-});
-
-window.router.router.addEventListener("navigation-error", e => {
-  Nprogress.done();
-});
