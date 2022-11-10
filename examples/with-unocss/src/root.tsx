f491f27090f664b0a71d891f24003008dc718de6
Shoubhit Dash <shoubhit2005@gmail.com>
2022-11-10 15:31:31 +0530

    feat: put unocss in title

--- examples/with-unocss/src/root.tsx
+++ examples/with-unocss/src/root.tsx
@@ -20,7 +20,7 @@ export default function Root() {
   return (
     <Html lang="en">
       <Head>
-        <Title>SolidStart - Bare</Title>
+        <Title>SolidStart - UnoCSS</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
