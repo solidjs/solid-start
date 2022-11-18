d52d3c08d2bfba0198d4cbd5a4fc3699c0f46047
Amit Dahan <amitdahan11@gmail.com>
2022-11-18 20:41:10 +0200

    Resolve `solid-start/islands/entry-client` from user's CWD

--- packages/start/bin.cjs
+++ packages/start/bin.cjs
@@ -153,6 +153,7 @@ prog
     console.log(c.magenta(" version "), pkg.version);
 
     const config = await resolveConfig({ configFile, root, mode: "production", command: "build" });
+    const startPath = dirname(requireCwd.resolve('solid-start/package.json'));
 
     const { default: prepareManifest } = await import("./fs-router/manifest.js");
 
@@ -175,7 +176,7 @@ prog
             minify: process.env.START_MINIFY === "false" ? false : config.build?.minify ?? true,
             rollupOptions: {
               input: [
-                resolve(join(config.root, "node_modules", "solid-start", "islands", "entry-client"))
+                join(startPath, "islands", "entry-client")
               ],
               output: {
                 manualChunks: undefined
