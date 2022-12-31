31ad96cf3aae33c7e5a47c170c0ef11cd0f29f03
nirtamir2 <nirtamir2@gmail.com>
2022-12-31 10:32:37 +0200

    add solid-start routes command (#609)

--- packages/start/bin.cjs
+++ packages/start/bin.cjs
@@ -32,6 +32,25 @@ const findAny = (path, name) => {
 };
 
 prog
+  .command("routes").describe("Show all routes in your app")
+  .action(async ({config: configFile, open, port, root, host, inspect}) => {
+    root = root || process.cwd();
+    const config = await resolveConfig({ mode: "production", configFile, root, command: "build" });
+
+    const { Router } = await import("./fs-router/router.js");
+
+    const router = new Router({
+      baseDir: path.posix.join(config.solidOptions.appRoot, config.solidOptions.routesDir),
+      pageExtensions: config.solidOptions.pageExtensions,
+      ignore: config.solidOptions.routesIgnore,
+      cwd: config.solidOptions.root
+    });
+    await router.init();
+
+    console.log(JSON.stringify(router.getFlattenedPageRoutes(), null, 2));
+  });
+
+prog
   .command("dev")
   .describe("Start a development server")
   .option("-o, --open", "Open a browser tab", false)
