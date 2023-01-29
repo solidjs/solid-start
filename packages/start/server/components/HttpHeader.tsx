82901a8a21b24a90cbb740b304ba307d167e5d94
Sam C. Finan <samcfinan@gmail.com>
2023-01-29 04:00:42 -0500

    Append http headers instead of overwrite (#233)
    * Append http headers instead of overwrite
    
    * Set by default, append if prop set
    
    ---------
    
    Co-authored-by: Nikhil Saraf <nsaraf98@gmail.com>
    Co-authored-by: Ryan Carniato <ryansolid@gmail.com>

--- packages/start/server/components/HttpHeader.tsx
+++ packages/start/server/components/HttpHeader.tsx
@@ -2,16 +2,27 @@ import { onCleanup, useContext } from "solid-js";
 import { isServer } from "solid-js/web";
 import { ServerContext } from "../ServerContext";
 
-export function HttpHeader(props: { name: string; value: string }) {
+export function HttpHeader(props: { name: string; value: string; append?: boolean }) {
   const pageContext = useContext(ServerContext);
 
   if (isServer) {
-    pageContext!.responseHeaders.set(props.name, props.value);
+    if (props.append) {
+      pageContext!.responseHeaders.append(props.name, props.value);
+    } else {
+      pageContext!.responseHeaders.set(props.name, props.value);
+    }
   }
 
   onCleanup(() => {
     if (isServer) {
-      pageContext!.responseHeaders.delete(props.name);
+      const value = pageContext!.responseHeaders.get(props.name);
+      if (value) {
+        const values = value.split(", ");
+        const index = values.indexOf(props.value);
+        index !== -1 && values.splice(index, 1);
+        if (values.length) pageContext!.responseHeaders.set(props.name, values.join(", "));
+        else pageContext!.responseHeaders.delete(props.name);
+      }
     }
   });
 
