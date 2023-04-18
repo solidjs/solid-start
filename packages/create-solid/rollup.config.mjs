dd506e3f32ef08895b3259a8c45f4fbcf127c758
Ryan Carniato <ryansolid@gmail.com>
2023-04-18 16:13:23 -0700

    update build for CLI

--- packages/create-solid/rollup.config.mjs
+++ packages/create-solid/rollup.config.mjs
@@ -1,6 +1,7 @@
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
+import { builtinModules } from "module";
 
 export default {
   input: "cli/index.js",
@@ -8,9 +9,9 @@ export default {
     file: "bin",
     format: "cjs",
     banner: "#!/usr/bin/env node\nglobal.navigator={}",
-    interop: false,
+    interop: "default",
     inlineDynamicImports: true
   },
   plugins: [nodeResolve(), commonjs(), json()],
-  external: require("module").builtinModules
+  external: builtinModules
 };
