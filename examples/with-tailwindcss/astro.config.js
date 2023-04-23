919023d8dcea3c46898e428d86cf0eb31487e622
OrJDev <91349014+OrJDev@users.noreply.github.com>
2023-04-23 08:08:07 +0300

    fix: with tailwindcss example (#851)
    * fix: with tailwindcss example
    
    * reorder imports

--- examples/with-tailwindcss/astro.config.js
+++ examples/with-tailwindcss/astro.config.js
@@ -1,12 +1,13 @@
-import node from '@astrojs/node';
-import { defineConfig } from 'astro/config';
-import start from 'solid-start/astro';
+import node from "@astrojs/node";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+import start from "solid-start/astro";
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	adapter: node({
-		mode: 'standalone',
-	}),
-	integrations: [start()]
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  }),
+  integrations: [start(), tailwind()]
 });
