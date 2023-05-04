42c63981f201b929ba8b6611fa90a936fd03094b
Ryan Carniato <ryansolid@gmail.com>
2023-05-04 12:05:10 -0700

    update authjs example

--- examples/with-authjs/src/routes/api/auth/[...solidauth].ts
+++ examples/with-authjs/src/routes/api/auth/[...solidauth].ts
@@ -2,11 +2,13 @@ import DiscordProvider from "@auth/core/providers/discord";
 import { SolidAuth, SolidAuthConfig } from "@auth/solid-start";
 
 export const authOptions: SolidAuthConfig = {
+  secret: import.meta.env.AUTH_SECRET as string,
+  trustHost: import.meta.env.TRUST_HOST as boolean,
   providers: [
     // @ts-expect-error Types are wrong
     DiscordProvider({
-      clientId: process.env.DISCORD_CLIENT_ID as string,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET as string
+      clientId: import.meta.env.DISCORD_CLIENT_ID as string,
+      clientSecret: import.meta.env.DISCORD_CLIENT_SECRET as string
     })
   ]
 };
