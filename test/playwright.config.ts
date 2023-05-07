57416473a4e0ed622b966022580548f50496a689
Jess <jess@jessicasachs.io>
2023-05-07 04:07:52 -0400

    fix: Allow multiple instances of solid-start on the same computer. Should fix e2e tests. (#875)

--- test/playwright.config.ts
+++ test/playwright.config.ts
@@ -4,13 +4,13 @@ import { devices } from "@playwright/test";
 const config: PlaywrightTestConfig = {
   testDir: ".",
   testMatch: ["**/*-test.ts"],
-  timeout: 120_000,
+  fullyParallel: true,
+  timeout: process.env.CI ? 120_000 : 30_000, // 2 minutes in CI, 30 seconds locally
   expect: {
-    timeout: 5_000
+    timeout: 5_000 // 5 second retries for assertions
   },
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 3 : undefined,
   reporter: process.env.CI
     ? "github"
     : [["html", { open: process.env.TEST_REPORT ? "always" : "none" }]],
