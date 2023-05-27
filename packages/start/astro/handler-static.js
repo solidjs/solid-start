c7d9fd09d3df6009e78622c6e14e5842b8e86138
Ryan Carniato <ryansolid@gmail.com>
2023-05-26 17:02:58 -0700

    update astro

--- packages/start/astro/handler-static.js
+++ packages/start/astro/handler-static.js
@@ -15,7 +15,7 @@ async function handleRequest(req, handler, manifest) {
 }
 
 export default async req => {
-  const { _: { startHandler, routeManifest } } = await import(pathToFileURL(process.env.START_ENTRY_STATIC).toString());
+  const { startHandler, routeManifest } = await import(pathToFileURL(process.env.START_ENTRY_STATIC).toString());
   let webRes = await handleRequest(req, startHandler, process.env.START_INDEX_HTML ? {} : routeManifest);
   if (webRes.status === 200) {
     return webRes.text();
