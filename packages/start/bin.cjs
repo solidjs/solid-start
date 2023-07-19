8155e7021aac2cfdfe667062c872f79fb1c20bf9
Davide <43080019+edivados@users.noreply.github.com>
2023-07-19 21:30:19 +0200

    fix SPA client build failing because of whitespace in path (#926)

--- packages/start/bin.cjs
+++ packages/start/bin.cjs
@@ -373,7 +373,7 @@ prog
               "dev",
               "--mode",
               "production",
-              ...(config ? ["--config", config.configFile] : []),
+              ...(config ? ["--config", `"${config.configFile}"`] : []),
               ...(port ? ["--port", port] : [])
             ],
             {
