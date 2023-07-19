cecd048b8bc8844f9660a8fdbbc9b3a35fe336b5
Davide <43080019+edivados@users.noreply.github.com>
2023-07-19 23:41:04 +0200

    [astro] Docs: Fix md rendering and build (#929)
    * fix md
    
    * fix prod build

--- astro.config.js
+++ astro.config.js
@@ -1,7 +1,7 @@
 import node from '@astrojs/node';
-import mdx from "solid-start-mdx";
 import { defineConfig } from 'astro/config';
 import { resolve } from "path";
+import mdx from "solid-start-mdx";
 import start from 'solid-start/astro';
 
 // https://astro.build/config
@@ -19,12 +19,21 @@ export default defineConfig({
     optimizeDeps: {
       entries: []
     },
-    plugins: [await mdx()],
+    plugins: [
+      await mdx(), 
+      {
+        name: "remove:astro:markdown",
+        enforce: "pre",
+        configResolved(c) {
+          const astro = c.plugins.findIndex(p => p.name === "astro:markdown");
+          if (astro !== -1) c.plugins.splice(astro, 1);
+        }
+      }
+    ],
   },
 	integrations: [start({
     rootEntry: resolve("docs.root.tsx"),
-    appRoot: "./docs",
-    routesDir: ".",
+    routesDir: "../docs",
     islandsRouter: true,
     islands: true,
     extensions: [".mdx", ".md"],
