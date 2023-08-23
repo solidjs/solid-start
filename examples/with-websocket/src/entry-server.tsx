6d4eb2bbf8a97737c09a466c46470ba7a489bfd7
cin <eryue0220@gmail.com>
2023-08-23 10:55:34 +0800

    fix: websocket example bug (#1015)
    Co-authored-by: Ryan Carniato <ryansolid@gmail.com>

--- examples/with-websocket/src/entry-server.tsx
+++ examples/with-websocket/src/entry-server.tsx
@@ -1,3 +1,7 @@
+import { createDurableObject } from "solid-start/durable-object";
 import { createHandler, renderAsync, StartServer } from "solid-start/entry-server";
+import ws from 'solid-start/websocket/handler';
+
+export const DO_WEBSOCKET = createDurableObject(ws);
 
 export default createHandler(renderAsync(event => <StartServer event={event} />));
