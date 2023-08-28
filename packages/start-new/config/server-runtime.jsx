7f06b96e1992d03bea4090be04ea47557a1dab64
Nikhil Saraf <nsaraf98@gmail.com>
2023-08-29 00:00:03 +0530

    handle non function exports from use client

--- packages/start-new/config/server-runtime.jsx
+++ packages/start-new/config/server-runtime.jsx
@@ -25,19 +25,9 @@ export function createServerReference(fn, id, name) {
 }
 
 export function createClientReference(Component, id, name) {
-  // function Island(props) {
-  //   console.log(props);
-  //   return <solid-island data-component={id}>{createComponent(Component, props)}</solid-island>;
-  // }
+  if (typeof Component === "function") {
+    return createIslandReference(Component, id, name);
+  }
 
-  return createIslandReference(Component, id, name);
-  // return new Proxy(fn, {
-  //   get(target, prop, receiver) {
-  //     console.log(target, prop);
-  //   },
-  //   apply(target, thisArg, args) {
-  //     // console.log("Client references are not supported yet");
-  //     // throw new Error("Client references are not supported yet");
-  //   }
-  // });
+  return Component;
 }
