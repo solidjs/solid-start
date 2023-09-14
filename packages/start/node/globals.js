e07a9e907731e65c24e48b6f20fafc354b3f06a6
pan93412 <pan93412@gmail.com>
2023-09-15 04:20:41 +0800

    feat(start/node): Don't `Object.assign` on Bun (#1060)
    Co-authored-by: Ryan Carniato <ryansolid@gmail.com>

--- packages/start/node/globals.js
+++ packages/start/node/globals.js
@@ -1,7 +1,14 @@
 import crypto from "crypto";
 import Streams from "stream/web";
 
-Object.assign(globalThis, Streams);
+// Bun does not support this assignment.
+// We only assign globalThis if the runtime
+// is not Bun.
+//
+// https://bun.sh/guides/util/detect-bun
+if (!process.versions.bun) {
+  Object.assign(globalThis, Streams);
+}
 
 if (globalThis.crypto != crypto.webcrypto) {
   // @ts-ignore
