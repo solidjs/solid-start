7a8b665800b1b3702325fdd71cca62554bf7fae6
Nikhil Saraf <nsaraf98@gmail.com>
2023-09-16 13:16:39 +0530

    middleware early return

--- examples/bare/src/middleware.ts
+++ examples/bare/src/middleware.ts
@@ -1,6 +1,9 @@
 import { createMiddleware } from "@solidjs/start/server";
+import { sendWebResponse } from "vinxi/runtime/server";
 
 export default createMiddleware({
-  onRequest: console.log,
+  onRequest: event => {
+    sendWebResponse(event, new Response("Hello World!"));
+  },
   onBeforeResponse: console.log
 });
