eb8110227d527f693314bf144689c807b32b53d7
Ryan Carniato <ryansolid@gmail.com>
2023-09-21 15:05:59 -0700

    clean up hackernews example

--- examples/hackernews/vite.config.js
+++ examples/hackernews/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
   start: {
-    islands: true,
-    middleware: "./src/middleware.ts"
+    islands: true
   }
 });
