f42ac555393f112d5bf75b4fb572393d51c1f5f0
Nikhil Saraf <nsaraf98@gmail.com>
2023-10-28 15:02:41 +0530

    add url to server-runtime, needs to be supported in the backend too

--- packages/start-new/config/server-runtime.jsx
+++ packages/start-new/config/server-runtime.jsx
@@ -16,6 +16,9 @@ async function fetchServerAction(base, id, args) {
 export function createServerReference(fn, id, name) {
   return new Proxy(fn, {
     get(target, prop, receiver) {
+      if (prop === "url") {
+        return `/_server?id=${encodeURIComponent(id)}&name=${encodeURIComponent(name)}`;
+      }
     },
     apply(target, thisArg, args) {
       return fetchServerAction("/_server", `${id}#${name}`, args);
