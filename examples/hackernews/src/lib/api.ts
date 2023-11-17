94e434e88862a2186d0816692580abb12bddaf7a
Ryan Carniato <ryansolid@gmail.com>
2023-11-16 16:25:24 -0800

    update router, fix up hackernews

--- examples/hackernews/src/lib/api.ts
+++ examples/hackernews/src/lib/api.ts
@@ -10,6 +10,6 @@ const mapStories = {
   job: "jobs"
 } as const;
 
-export const getStories = cache(async (type: StoryTypes, page: number): Promise<Story[]> => fetchAPI(`${mapStories[type]}?page=${page}`), "stories", {});
-export const getStory = cache(async (id: string): Promise<Story> => fetchAPI(`item/${id}`), "story", {});
-export const getUser = cache(async (id: string): Promise<User> => fetchAPI(`user/${id}`), "user", {});
+export const getStories = cache(async (type: StoryTypes, page: number): Promise<Story[]> => fetchAPI(`${mapStories[type]}?page=${page}`), "stories");
+export const getStory = cache(async (id: string): Promise<Story> => fetchAPI(`item/${id}`), "story");
+export const getUser = cache(async (id: string): Promise<User> => fetchAPI(`user/${id}`), "user");
