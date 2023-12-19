cffad3fb4348996afcde178c02aaa21da942979b
Ryan Carniato <ryansolid@gmail.com>
2023-12-19 12:38:00 -0800

    change templates to be ssr true by default

--- examples/bare/vite.config.ts
+++ examples/bare/vite.config.ts
@@ -1,3 +1,3 @@
 import { defineConfig } from "@solidjs/start/config";
 
-export default defineConfig({ start: { ssr: false } });
+export default defineConfig({});
--- examples/experiments/vite.config.ts
+++ examples/experiments/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
   start: {
-    ssr: true,
     middleware: "./src/middleware.ts"
   }
 });
