ec0b8eb0173a9051951491700c1ee6bb5efe6662
Vladimir Kutepov <frenzzy.man@gmail.com>
2024-01-03 12:23:16 +0300

    style obj -> style str

--- packages/start/shared/ErrorBoundary.tsx
+++ packages/start/shared/ErrorBoundary.tsx
@@ -18,37 +18,35 @@ function ErrorMessage(props: { error: any }) {
   createEffect(() => console.error(props.error));
 
   return (
-    <div style={{ padding: '16px' }}>
+    <div style="padding:16px">
       <HttpStatusCode code={500} />
       <div
-        style={{
-          'background-color': 'rgb(252,165,165)',
-          color: 'rgb(153,27,27)',
-          'border-radius': '5px',
-          overflow: 'scroll',
-          padding: '16px',
-          'margin-bottom': '8px',
-        }}
+        style={
+          'background-color:#fca5a5;' +
+          'color:#991b1b;' +
+          'border-radius:5px;' +
+          'overflow:scroll;' +
+          'padding:16px;' +
+          'margin-bottom:8px'
+        }
       >
-        <p style={{ 'font-weight': 'bold' }} id="error-message">
+        <p style="font-weight:bold" id="error-message">
           {props.error.message}
         </p>
         <button
           id="reset-errors"
           onClick={resetErrorBoundaries}
-          style={{
-            color: 'rgb(252,165,165)',
-            'background-color': 'rgb(153,27,27)',
-            'border-radius': '5px',
-            padding: '4px 8px',
-            'margin-top': '8px',
-          }}
+          style={
+            'color:#fca5a5;' +
+            'background-color:#991b1b;' +
+            'border-radius:5px;' +
+            'padding:4px 8px;' +
+            'margin-top:8px'
+          }
         >
           Clear errors and retry
         </button>
-        <pre style={{ 'margin-top': '8px', width: '100%' }}>
-          {props.error.stack}
-        </pre>
+        <pre style="margin-top:8px;width:100%">{props.error.stack}</pre>
       </div>
     </div>
   );
