33620f7067649ea8e63d0506e71c3a31518adb67
marvin-j97 <marvin.janke.97@gmail.com>
2024-01-15 21:15:57 +0100

    feat: add unocss example

---
+++ examples/with-unocss/src/components/Nav.tsx
@@ -0,0 +1,21 @@
+import { useLocation } from "@solidjs/router";
+
+export default function Nav() {
+  const location = useLocation();
+  const active = (path: string) =>
+    path == location.pathname
+      ? "border-sky-600"
+      : "border-transparent hover:border-sky-600";
+  return (
+    <nav class="bg-sky-800">
+      <ul class="container flex items-center p-3 text-gray-200">
+        <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
+          <a href="/">Home</a>
+        </li>
+        <li class={`border-b-2 ${active("/about")} mx-1.5 sm:mx-6`}>
+          <a href="/about">About</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
