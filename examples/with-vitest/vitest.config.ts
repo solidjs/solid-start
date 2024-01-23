f7d94cfe26d869c38bf457bd702ca68420ac022d
Alex Lohr <alex.lohr@logmein.com>
2024-01-23 10:34:36 +0100

    vitest example: reduced configuration

--- examples/with-vitest/vitest.config.ts
+++ examples/with-vitest/vitest.config.ts
@@ -3,19 +3,6 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [solid()],
-  test: {
-    deps: {
-      optimizer: {
-        web: {
-          enabled: true
-        }
-      }
-    },
-    environment: "jsdom",
-    globals: true,
-    setupFiles: ['./setupVitest.ts'],
-    testTransformMode: { web: ["/\.[jt]sx?$/"] },
-  },
   resolve: {
     conditions: ["development", "browser"],
   },
