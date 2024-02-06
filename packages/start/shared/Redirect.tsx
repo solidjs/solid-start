90f45894694e118e5db7c8888e7804cfbe6af24d
Marvin <33938500+marvin-j97@users.noreply.github.com>
2024-02-06 18:50:27 +0100

    Fix Redirect permanent prop type

--- packages/start/shared/Redirect.tsx
+++ packages/start/shared/Redirect.tsx
@@ -1,7 +1,7 @@
 import { HttpHeader } from "./HttpHeader";
 import { HttpStatusCode } from "./HttpStatusCode";
 
-export function Redirect(props: { to: string, permanent: boolean }) {
+export function Redirect(props: { to: string, permanent?: boolean }) {
   return <>
     <HttpHeader name="location" value={props.to} />
     <HttpStatusCode code={props.permanent ? 308 : 307} />
