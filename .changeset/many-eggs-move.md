4d66b4de053ad3c2cdafa89d4ec2e682a09309bc
Paul Armstrong <me@paularmstrong.dev>
2024-02-06 08:40:18 -0800

    fix: ensure ParentProps is declared as a type import
    Fixes #1311

---
+++ .changeset/many-eggs-move.md
@@ -0,0 +1,5 @@
+---
+"@solidjs/start": patch
+---
+
+Fixed an issue with TypeScript based projects throwing client side errors regarding a type input in the `ErrorBoundary`.wq
--- packages/start/shared/ErrorBoundary.tsx
+++ packages/start/shared/ErrorBoundary.tsx
@@ -1,24 +1,13 @@
-import {
-  ErrorBoundary as DefaultErrorBoundary,
-  ParentProps,
-} from "solid-js";
+import { ErrorBoundary as DefaultErrorBoundary, type ParentProps } from "solid-js";
 import { HttpStatusCode } from "./HttpStatusCode";
 import { DevOverlay } from "./dev-overlay";
 
 export function ErrorBoundary(props: ParentProps) {
   if (import.meta.env.DEV) {
-    return (
-      <DevOverlay>
-        {props.children}
-      </DevOverlay>
-    );
+    return <DevOverlay>{props.children}</DevOverlay>;
   }
   return (
-    <DefaultErrorBoundary
-      fallback={(
-        <HttpStatusCode code={500} />
-      )}
-    >
+    <DefaultErrorBoundary fallback={<HttpStatusCode code={500} />}>
       {props.children}
     </DefaultErrorBoundary>
   );
