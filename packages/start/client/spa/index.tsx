1c6deeef7284d2ce8d5854f752e255423c1dbd48
Ryan Carniato <ryansolid@gmail.com>
2024-02-13 20:45:02 -0800

    fix types

--- packages/start/client/spa/index.tsx
+++ packages/start/client/spa/index.tsx
@@ -1,8 +1,8 @@
 import type { JSX } from "solid-js";
-import { render } from "solid-js/web";
+import { render, type MountableElement } from "solid-js/web";
 import "vinxi/client";
 
-export function mount(fn: () => JSX.Element, el: HTMLElement) {
+export function mount(fn: () => JSX.Element, el: MountableElement) {
   render(fn, el);
 }
 
