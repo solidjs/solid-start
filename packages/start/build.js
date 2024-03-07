5a33aa8535cd50dd333423321b7ab0176be5df05
Brendan Allan <brendonovich@outlook.com>
2024-03-07 19:31:51 +0800

    use tsc to build .d.ts

---
+++ packages/start/build.js
@@ -0,0 +1,12 @@
+import {glob} from "glob";
+import {copyFile, mkdir} from "node:fs/promises"
+import { join, dirname } from "node:path";
+
+const SOURCE_FOLDERS = ["client", "config", "middleware", "server", "shared"]
+
+const assets = [...await glob(`./{${SOURCE_FOLDERS.join(",")}}/**/*.css`), "./package.json"]
+
+await Promise.all(assets.map(async a => {
+	await mkdir(join("dist", dirname(a)), { recursive: true })
+	await copyFile(a, `dist/${a}`);
+}))
