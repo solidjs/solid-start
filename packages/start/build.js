1678bf23400eb04fe2ad99ee46dc93cea65fc144
Brendan Allan <brendonovich@outlook.com>
2024-03-09 11:58:01 +0800

    add router folder to build script

--- packages/start/build.js
+++ packages/start/build.js
@@ -2,7 +2,7 @@ import {glob} from "glob";
 import {copyFile, mkdir} from "node:fs/promises"
 import { join, dirname } from "node:path";
 
-const SOURCE_FOLDERS = ["client", "config", "middleware", "server", "shared"]
+const SOURCE_FOLDERS = ["client", "config", "middleware", "server", "shared", "router"]
 
 const assets = [...await glob(`./{${SOURCE_FOLDERS.join(",")}}/**/*.css`), "./package.json"]
 
