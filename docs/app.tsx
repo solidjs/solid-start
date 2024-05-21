e1e8351948e2e304d3415a85f03b4c030c7e12c9
Daniel Afonso <danieljcafonso@gmail.com>
2024-05-21 17:36:30 +0100

    feat: update og image

--- docs/app.tsx
+++ docs/app.tsx
@@ -1,5 +1,9 @@
 // @refresh reload
-import { ColorModeProvider, ColorModeScript, cookieStorageManagerSSR } from "@kobalte/core/color-mode";
+import {
+  ColorModeProvider,
+  ColorModeScript,
+  cookieStorageManagerSSR
+} from "@kobalte/core/color-mode";
 import { Meta, MetaProvider, Title } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
@@ -41,8 +45,8 @@ export default function App() {
             <Meta property="og:type" content="website" />
             <Meta name="twitter:card" content="summary_large_image" />
             <Meta name="twitter:site" content="@solid_js" />
-            <Meta property="og:image" content="https://start.solidjs.com/logo.svg" />
-            <Meta property="twitter:image" content="https://start.solidjs.com/logo.svg" />
+            <Meta property="og:image" content="https://start.solidjs.com/start_og.png" />
+            <Meta property="twitter:image" content="https://start.solidjs.com/start_og.png" />
             <Suspense>{props.children}</Suspense>
           </ColorModeProvider>
         </MetaProvider>
