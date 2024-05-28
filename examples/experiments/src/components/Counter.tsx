75fcac6c41d285f255cdb0f4cd29c4c8040e479d
Hamir Mahal <hamirmahal@gmail.com>
2024-05-28 16:47:10 -0700

    fix: ambiguous attribute for `button`

--- examples/experiments/src/components/Counter.tsx
+++ examples/experiments/src/components/Counter.tsx
@@ -7,7 +7,7 @@ import counterContext from "./counterContext";
 export default function Counter() {
   const [count, setCount] = useContext(counterContext);
   return (
-    <button class="increment" onClick={() => setCount(count() + 1)}>
+    <button class="increment" onClick={() => setCount(count() + 1)} type="button">
       Clicks: {count()}
     </button>
   );
