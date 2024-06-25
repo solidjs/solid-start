6352aa02375da80fa001d6efcb4b1809600aef1c
thednp <thednp@users.noreply.github.com>
2024-06-25 23:08:59 +0300

    Update drizzle dependencies and config (#1558)
    * Update deps
    
    * Update drizzle.config.ts
    
    ---------
    
    Co-authored-by: Ryan Carniato <ryansolid@gmail.com>

--- examples/with-drizzle/drizzle.config.ts
+++ examples/with-drizzle/drizzle.config.ts
@@ -1,9 +1,8 @@
-// import { type Config } from "drizzle-kit";
- 
 export default {
+  dialect: "sqlite",
   schema: "./drizzle/schema.ts",
   out: "./drizzle/migrations/",
-  driver: "better-sqlite",
+  // driver: "better-sqlite",
   dbCredentials: {
     url: './drizzle/db.sqlite',
   },
