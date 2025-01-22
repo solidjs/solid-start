fe73291d78d0f249d0f5b02bbc45fc2954a01110
Birk Skyum <birk.skyum@pm.me>
2025-01-22 21:23:20 +0100

    add cypress server-function test

---
+++ cypress.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "cypress";
+// import { createRequire } from "module";
+// const require = createRequire(import.meta.url);
+
+export default defineConfig({
+  env: {
+    codeCoverage: {
+      exclude: "cypress/**/*.*",
+    },
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      // require("@cypress/code-coverage/task")(on, config);
+      return config;
+    },
+    baseUrl: "http://localhost:3000",
+    retries: {
+      runMode: 2,
+      openMode: 0,
+    },
+  },
+});
