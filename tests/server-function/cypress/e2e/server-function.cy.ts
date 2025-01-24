6e03e2ab1b43f61b50697fb8dbf1c752a9888d8b
Birk Skyum <birk.skyum@pm.me>
2025-01-24 16:30:35 +0100

    Expand server function test

--- tests/server-function/cypress/e2e/server-function.cy.ts
+++ tests/server-function/cypress/e2e/server-function.cy.ts
@@ -1,6 +1,6 @@
 describe("server-function", () => {
   it("should isServer false on the client and true in the server function", () => {
     cy.visit("/");
-    cy.get("#server-fn-test").contains(`{"client":false,"serverFn":true}`);
+    cy.get("#server-fn-test").contains(`{"clientWithIsServer":false,"serverFnWithIsServer":true,"serverFnWithNodeBuiltin":"can/externalize"}`);
   })
 });
--- tests/server-function/src/app.tsx
+++ tests/server-function/src/app.tsx
@@ -1,19 +1,34 @@
+import { join } from 'path';
 import { createEffect, createSignal } from "solid-js";
 import { isServer } from "solid-js/web";
+
 import "./app.css";
 
-function useServer() {
+function serverFnWithIsServer() {
   "use server";
+
   return isServer;
 }
-export default function App() {
-  const [output, setOutput] = createSignal<{ client?: boolean; serverFn?: boolean }>({});
 
-  setOutput(prev => ({ ...prev, client: isServer }));
+function serverFnWithNodeBuiltin() {
+  "use server";
+
+  return join('can','externalize');
+}
+
+export default function App() {
+  const [output, setOutput] = createSignal<{ clientWithIsServer?: boolean; serverFnWithIsServer?: boolean, serverFnWithNodeBuiltin?: string }>({});
+
+  setOutput(prev => ({ ...prev, clientWithIsServer: isServer }));
 
   createEffect(async () => {
-    const restult = await useServer();
-    setOutput(prev => ({ ...prev, serverFn: restult }));
+    const restult = await serverFnWithIsServer();
+    setOutput(prev => ({ ...prev, serverFnWithIsServer: restult }));
+  });
+
+  createEffect(async () => {
+    const restult = await serverFnWithNodeBuiltin();
+    setOutput(prev => ({ ...prev, serverFnWithNodeBuiltin: restult }));
   });
 
   return (
