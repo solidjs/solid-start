dbbcb70d239abcc7665a1243f71f86b40dc55d31
Brian McKelvey <theturtle32@gmail.com>
2025-05-02 16:24:59 -0700

    Adding failing test for issue #1886

---
+++ packages/tests/src/routes/generator-server-function.tsx
@@ -0,0 +1,19 @@
+import { createSignal, onMount } from "solid-js";
+import { sayHello } from '~/functions/use-generator-server-function';
+
+export default function GeneratorServerFunction() {
+  const [output, setOutput] = createSignal<string>('');
+
+  onMount(async () => {
+    const greetings = await sayHello();
+    for await (const greeting of greetings) {
+      setOutput(greeting);
+    }
+  });
+
+  return (
+    <main>
+      <div id="server-fn-test">{output()}</div>
+    </main>
+  );
+}
