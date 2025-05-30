f4052b2532b54ba09b74f566f6502e0ae522a255
jer3m01 <jer3m01@jer3m01.com>
2025-05-30 19:11:09 +0200

    cli: add with-solidbase example (#1847)

---
+++ examples/with-solidbase/app.config.ts
@@ -0,0 +1,41 @@
+import { defineConfig } from "@solidjs/start/config";
+import { withSolidBase } from "@kobalte/solidbase/config";
+
+export default defineConfig(withSolidBase(
+  // SolidStart config
+  {
+    server: {
+      prerender: {
+        crawlLinks: true
+      }
+    }
+  },
+  // SolidBase config
+  {
+    title: "SolidBase",
+    titleTemplate: ":title - SolidBase",
+    description: "Fully featured, fully customisable static site generation for SolidStart",
+    themeConfig: {
+      sidebar: {
+        "/": {
+          items: [
+            {
+              title: "Overview",
+              collapsed: false,
+              items: [
+                {
+                  title: "Home",
+                  link: "/"
+                },
+                {
+                  title: "About",
+                  link: "/about"
+                }
+              ]
+            }
+          ]
+        }
+      }
+    }
+  }
+));
