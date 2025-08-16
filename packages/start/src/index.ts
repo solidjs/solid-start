4d89def0755dc90126f203a0dd35976d3b3fadb3
Brendan Allan <brendonovich@outlook.com>
2025-08-16 16:20:31 +0800

    fix build

--- packages/start/src/index.ts
+++ packages/start/src/index.ts
@@ -1,6 +1,7 @@
 // @refresh skip
-export { default as clientOnly } from "./shared/clientOnly.js";
+
 export type {
   APIEvent,
   APIHandler, Asset, ContextMatches, DocumentComponentProps, FetchEvent, HandlerOptions, PageEvent, ResponseStub, ServerFunctionMeta
-} from "./types.js";
+} from "./server/types.js";
+export { default as clientOnly } from "./shared/clientOnly.js";
