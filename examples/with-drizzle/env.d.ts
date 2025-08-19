114cfeeb0448592da5bdfb45c92507ea0259fde0
Brenley Dueck <brenleydueck@gmail.com>
2025-08-18 20:01:03 -0500

    remove some vinxi types

--- examples/with-drizzle/env.d.ts
+++ examples/with-drizzle/env.d.ts
@@ -1,12 +1,10 @@
-/// <reference types="vinxi/types/client" />
-
 interface ImportMetaEnv {
   DB_URL: string;
   DB_MIGRATIONS_URL: string;
   SITE_NAME: string;
   SESSION_SECRET: string;
 }
-  
+
 interface ImportMeta {
-  readonly env: ImportMetaEnv
+  readonly env: ImportMetaEnv;
 }
