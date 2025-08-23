9bdb7c808ced2a1b3af4269b3f31a66ebb7034c1
Brenley Dueck <brenleydueck@gmail.com>
2025-08-22 21:43:36 -0500

    Server function meta (#1951)

--- packages/start/src/index.ts
+++ packages/start/src/index.ts
@@ -2,7 +2,18 @@
 
 export type {
   APIEvent,
-  APIHandler, Asset, ContextMatches, DocumentComponentProps, FetchEvent, HandlerOptions, PageEvent, ResponseStub, ServerFunctionMeta
+  APIHandler,
+  Asset,
+  ContextMatches,
+  DocumentComponentProps,
+  FetchEvent,
+  HandlerOptions,
+  PageEvent,
+  ResponseStub,
+  ServerFunctionMeta
 } from "./server/types.js";
 export { default as clientOnly } from "./shared/clientOnly.js";
 export { HttpStatusCode } from "./shared/HttpStatusCode.js";
+
+export { GET } from "./shared/GET.js";
+export { getServerFunctionMeta } from "./shared/serverFunction";
