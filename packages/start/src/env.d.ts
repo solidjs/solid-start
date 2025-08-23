2094aa7678fdcf300888da54846bdf5316346899
Brendan Allan <brendonovich@outlook.com>
2025-08-23 14:29:54 +0800

    cleaup

--- packages/start/src/env.d.ts
+++ packages/start/src/env.d.ts
@@ -1,4 +1,3 @@
-/// <reference types="vite" />
 // This file is an augmentation to the built-in ImportMeta interface
 // Thus cannot contain any top-level imports
 // <https://www.typescriptlang.org/docs/handbook/declaration-merging.html#module-augmentation>
