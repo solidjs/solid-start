41fdd9658ec2bc2d4425b6987310a6681f819b5e
Brenley Dueck <brenleydueck@gmail.com>
2025-08-23 20:34:11 -0500

    fix ssr style css handling

--- packages/start/src/server/manifest/prod-server-manifest.ts
+++ packages/start/src/server/manifest/prod-server-manifest.ts
@@ -1,5 +1,5 @@
-import { manifest } from "solid-start:server-manifest"
 import { join } from "pathe";
+import { manifest } from "solid-start:server-manifest";
 
 export function getSsrProdManifest() {
   const viteManifest = manifest.clientViteManifest;
@@ -8,22 +8,22 @@ export function getSsrProdManifest() {
       return import(/* @vite-ignore */ id);
     },
     async getAssets(id) {
-      return []
+      return manifest.clientManifestData[id]?.assets ?? [];
     },
     async json() {
       const json: Record<string, any> = {};
 
       const entryKeys = Object.keys(viteManifest)
-        .filter((id) => viteManifest[id]?.isEntry)
-        .map((id) => id);
+        .filter(id => viteManifest[id]?.isEntry)
+        .map(id => id);
 
       for (const entryKey of entryKeys) {
         json[entryKey] = {
           output: join(viteManifest[entryKey]!.file)
-        }
+        };
       }
 
-      return json
+      return json;
     }
   } satisfies StartManifest & { json(): Promise<Record<string, any>> };
 }
