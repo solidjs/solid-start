781a7881cc6ae2fa2ca7f05df8a69ae18035591a
Brendan Allan <brendonovich@outlook.com>
2025-10-27 17:47:14 +0800

    copy css during build

---
+++ packages/start/scripts/build.js
@@ -0,0 +1,15 @@
+import * as fs from "node:fs/promises";
+import * as path from "node:path";
+import glob from "fast-glob";
+
+const files = await glob("**/*.{js,css}", { cwd: `${process.cwd()}/src` });
+
+await Promise.all(
+  files.map(file =>
+    fs.cp(
+      path.join(import.meta.dirname, "../src", file),
+      path.join(import.meta.dirname, "../dist", file),
+      { recursive: true }
+    )
+  )
+);
