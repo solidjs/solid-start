7b325b929ae39e7f7afa47b642958bd640afdaed
Oscar Beaumont <oscar@otbeaumont.me>
2025-11-07 15:22:41 +0800

    bump versions

--- apps/fixtures/hackernews/vite.config.ts
+++ apps/fixtures/hackernews/vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 
 import { solidStart } from "../../../packages/start/src/config";
 import { nitroV2Plugin } from "../../../packages/start-nitro-v2-vite-plugin/src";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
-	plugins: [solidStart()],
+	plugins: [solidStart(), nitro()],
 });
