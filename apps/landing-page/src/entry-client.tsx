ef11bc680ddd6852e8b876acd40d3c37b63b5df3
Atila Fassina <atila@fassina.eu>
2025-11-20 18:44:52 +0100

    update lp setup

--- apps/landing-page/src/entry-client.tsx
+++ apps/landing-page/src/entry-client.tsx
@@ -1,3 +1,3 @@
-import { mount, StartClient } from "@solidjs/start/client";
+import { mount, StartClient } from "../../../packages/start/src/client";
 
 mount(() => <StartClient />, document.getElementById("app")!);
