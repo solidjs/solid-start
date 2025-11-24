e0c0eff208dd29d9ba8e640d4caf0fbc8d2ca33b
Brenley Dueck <brenleydueck@gmail.com>
2025-11-23 21:58:41 -0600

    feat: add back HttpHeader (#2017)
    Co-authored-by: Brendan Allan <brendonovich@outlook.com>

---
+++ apps/tests/src/routes/http-header.tsx
@@ -0,0 +1,10 @@
+import { HttpHeader } from "@solidjs/start";
+
+export default function HttpHeaderRoute() {
+    return (
+        <main>
+            <h1>Http Header</h1>
+            <HttpHeader name="test-header" value="test-value" />
+        </main>
+    );
+}
