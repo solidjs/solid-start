744f6044c18fc4acdcb61e0dea87e8cf6383add3
Brendan Allan <brendonovich@outlook.com>
2025-12-18 21:07:19 +0800

    reference vite/client in global env and modify fixture tsconfigs

--- packages/start/env.d.ts
+++ packages/start/env.d.ts
@@ -1,5 +1,7 @@
 // This file contains global type definitions that are exported as @solidjs/start/env
 
+/// <reference types="vite/client" />
+
 declare namespace App {
   export interface RequestEventLocals {
     [key: string | symbol]: any;
