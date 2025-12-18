78165d82609c5608c153e9d44933bc77b8bc5933
Brendan Allan <brendonovich@outlook.com>
2025-12-18 18:23:43 +0800

    export server types from /server

--- packages/start/src/index.tsx
+++ packages/start/src/index.tsx
@@ -1,18 +1,5 @@
 // @refresh skip
 
-export type {
-  APIEvent,
-  APIHandler,
-  Asset,
-  ContextMatches,
-  DocumentComponentProps,
-  FetchEvent,
-  HandlerOptions,
-  PageEvent,
-  ResponseStub,
-  ServerFunctionMeta,
-} from "./server/types.ts";
-
 export { default as clientOnly } from "./shared/clientOnly.ts";
 export { GET } from "./shared/GET.ts";
 export { HttpHeader } from "./shared/HttpHeader.tsx";
