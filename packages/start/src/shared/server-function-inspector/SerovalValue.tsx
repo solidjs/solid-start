471d0b5eba07cf8f969a83196d2e62b8f5d6bf11
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2026-01-18 23:45:06 +0800

    Add `FormDataViewer`

---
+++ packages/start/src/shared/server-function-inspector/SerovalValue.tsx
@@ -0,0 +1,14 @@
+import { Text } from "../ui/Text.tsx";
+import './SerovalValue.css';
+
+interface SerovalValueProps {
+  value: string | number | boolean | undefined | null;
+}
+
+export function SerovalValue(props: SerovalValueProps) {
+  return (
+    <Text data-start-seroval-value options={{ size: 'xs', weight: 'semibold', wrap: 'nowrap' }}>
+      {`${props.value}`}
+    </Text>
+  );
+}
