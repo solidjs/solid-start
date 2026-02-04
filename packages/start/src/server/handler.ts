8fe1e787593f80ba119e135b2e9dfe31ca6c4180
huseeiin <husseinfadhil651@gmail.com>
2026-02-04 15:59:00 +0300

    update comment

--- packages/start/src/server/handler.ts
+++ packages/start/src/server/handler.ts
@@ -114,7 +114,7 @@ export function createBaseHandler(
 
       // using TransformStream in dev can cause solid-start-dev-server to crash
       // when stream is cancelled
-      // send fast node streams (for now this is only available in nodejs)
+      // send fast node streams
       // @ts-expect-error
       if (e.runtime?.name === "node") return new FastResponse(stream);
 
