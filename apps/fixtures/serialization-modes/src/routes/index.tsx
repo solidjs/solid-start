950405a1a315cf2cf7b6e31d9765981a29306d76
Atila Fassina <atila@fassina.eu>
2026-02-21 11:19:58 +0100

    remove more useless code

--- apps/fixtures/serialization-modes/src/routes/index.tsx
+++ apps/fixtures/serialization-modes/src/routes/index.tsx
@@ -2,14 +2,6 @@ import { Title } from "@solidjs/meta";
 import { createEffect } from "solid-js";
 import Counter from "~/components/Counter";
 
-// const getDate = action(async () => {
-//   "use server";
-
-//   console.log("Page query executed");
-
-//   return new Date();
-// }, "date");
-
 const breakval = () => {
   "use server";
 
@@ -21,22 +13,11 @@ export default function Home() {
     console.log(breakval());
   });
 
-  // setTimeout(() => {
-  //   eval("console.log('This should not execute')");
-  // }, 3000);
   return (
     <main>
       <Title>Hello World</Title>
       <h1>Hello world!</h1>
       <Counter />
-      {/* <form method="post" action={getDate}>
-        <button type="submit">Get current date</button>
-      </form>
-      <Suspense>
-        <Show when={data()} fallback={<p>Loading...</p>}>
-          <p>Current date: {String(data())}</p>
-        </Show>
-      </Suspense> */}
       <p>
         Visit{" "}
         <a href="https://start.solidjs.com" target="_blank">
