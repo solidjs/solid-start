ad6c4588ba20b93bd4fc690aaaa97f493fa25c99
Brenley Dueck <brenleydueck@gmail.com>
2026-03-05 17:17:22 -0600

    more hackernews tweaks

--- apps/fixtures/hackernews/src/routes/stories/[id].tsx
+++ apps/fixtures/hackernews/src/routes/stories/[id].tsx
@@ -1,5 +1,5 @@
-import { createAsync, type RouteDefinition, type RouteSectionProps } from "@solidjs/router";
-import { For, Show } from "solid-js";
+import { type RouteDefinition, type RouteSectionProps } from "@solidjs/router";
+import { For, Show, createMemo } from "solid-js";
 import Comment from "~/components/comment";
 import { getStory } from "~/lib/api";
 
@@ -10,31 +10,29 @@ export const route = {
 } satisfies RouteDefinition;
 
 export default function Story(props: RouteSectionProps) {
-  const story = createAsync(() => getStory(props.params.id));
+  const story = createMemo(() => getStory(props.params.id));
   return (
-    <Show when={story()}>
-      <div class="item-view">
-        <div class="item-view-header">
-          <a href={story()!.url} target="_blank">
-            <h1>{story()!.title}</h1>
-          </a>
-          <Show when={story()!.domain}>
-            <span class="host">({story()!.domain})</span>
-          </Show>
-          <p class="meta">
-            {story()!.points} points | by <a href={`/users/${story()!.user}`}>{story()!.user}</a>{" "}
-            {story()!.time_ago} ago
-          </p>
-        </div>
-        <div class="item-view-comments">
-          <p class="item-view-comments-header">
-            {story()!.comments_count ? story()!.comments_count + " comments" : "No comments yet."}
-          </p>
-          <ul class="comment-children">
-            <For each={story()!.comments}>{comment => <Comment comment={comment()} />}</For>
-          </ul>
-        </div>
+    <div class="item-view">
+      <div class="item-view-header">
+        <a href={story().url} target="_blank">
+          <h1>{story().title}</h1>
+        </a>
+        <Show when={story().domain}>
+          <span class="host">({story().domain})</span>
+        </Show>
+        <p class="meta">
+          {story().points} points | by <a href={`/users/${story().user}`}>{story().user}</a>{" "}
+          {story().time_ago} ago
+        </p>
       </div>
-    </Show>
+      <div class="item-view-comments">
+        <p class="item-view-comments-header">
+          {story().comments_count ? story().comments_count + " comments" : "No comments yet."}
+        </p>
+        <ul class="comment-children">
+          <For each={story().comments}>{comment => <Comment comment={comment()} />}</For>
+        </ul>
+      </div>
+    </div>
   );
 }
