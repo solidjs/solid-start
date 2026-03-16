928cf50cdcff2f01944a8c17a56a45c7cea7faa6
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2026-03-16 23:48:54 +0800

    feat(env): add runtime

--- apps/tests/src/functions/use-server-function-meta.ts
+++ apps/tests/src/functions/use-server-function-meta.ts
@@ -2,6 +2,9 @@
 
 import { getServerFunctionMeta } from "@solidjs/start";
 
+import { SERVER_EXAMPLE } from 'env:server';
+
 export function serverFnWithMeta() {
+  console.log(SERVER_EXAMPLE);
   return typeof getServerFunctionMeta()?.id;
 }
--- packages/start/src/config/env.ts
+++ packages/start/src/config/env.ts
@@ -1,7 +1,14 @@
 import { loadEnv, type Plugin } from "vite";
 
+const LOADERS = {
+  node: `export default key => process.env[key];`,
+  "cloudflare-workers": `import { env } from 'cloudflare:workers';export default key => env[key];`,
+  "netlify-edge": `export default key => Netlify.env.get(key);`,
+};
+
 export interface EnvPluginOptions {
   server?: {
+    runtime: keyof typeof LOADERS | (string & {});
     load?: () => Record<string, string>;
     prefix?: string;
   };
@@ -14,10 +21,22 @@ export interface EnvPluginOptions {
 const SERVER_ENV = "env:server";
 const CLIENT_ENV = "env:client";
 
+const SERVER_RUNTIME_ENV = `${SERVER_ENV}/runtime`;
+
+const SERVER_RUNTIME_LOADER = `${SERVER_RUNTIME_ENV}/loader`;
+
 const DEFAULT_SERVER_PREFIX = "SERVER_";
 const DEFAULT_CLIENT_PREFIX = "CLIENT_";
 
 const SERVER_ONLY_MODULE = `throw new Error('Attempt to load server-only environment variables in client runtime.');`;
+
+const SERVER_RUNTIME_CODE = `import load from '${SERVER_RUNTIME_LOADER}';
+
+export default new Proxy({}, {
+  get(_, key) {
+    return load(key);
+  },
+})`;
 
 function convertObjectToModule(object: Record<string, string>): string {
   let result = "";
@@ -32,6 +51,9 @@ export function envPlugin(options?: EnvPluginOptions): Plugin {
   let env: string;
   const serverPrefix = currentOptions.server?.prefix ?? DEFAULT_SERVER_PREFIX;
   const clientPrefix = currentOptions.client?.prefix ?? DEFAULT_CLIENT_PREFIX;
+  const runtime = options?.server?.runtime ?? "node";
+  const runtimeCode = runtime in LOADERS ? LOADERS[runtime as keyof typeof LOADERS] : runtime;
+
   return {
     name: "solid-start:env",
     enforce: "pre",
@@ -39,7 +61,12 @@ export function envPlugin(options?: EnvPluginOptions): Plugin {
       env = config.mode !== "production" ? "development" : "production";
     },
     resolveId(id) {
-      if (id === SERVER_ENV || id === CLIENT_ENV) {
+      if (
+        id === SERVER_ENV ||
+        id === CLIENT_ENV ||
+        id === SERVER_RUNTIME_ENV ||
+        SERVER_RUNTIME_LOADER
+      ) {
         return id;
       }
       return null;
@@ -51,14 +78,20 @@ export function envPlugin(options?: EnvPluginOptions): Plugin {
         }
         const vars = currentOptions.server?.load
           ? currentOptions.server.load()
-          : loadEnv(env, false, clientPrefix);
+          : loadEnv(env, false, serverPrefix);
         return convertObjectToModule(vars);
       }
       if (id === CLIENT_ENV) {
         const vars = currentOptions.client?.load
           ? currentOptions.client.load()
-          : loadEnv(env, false, serverPrefix);
+          : loadEnv(env, false, clientPrefix);
         return convertObjectToModule(vars);
+      }
+      if (id === SERVER_RUNTIME_LOADER) {
+        return runtimeCode;
+      }
+      if (id === SERVER_RUNTIME_ENV) {
+        return SERVER_RUNTIME_CODE;
       }
       return null;
     },
