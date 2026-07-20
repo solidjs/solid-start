e3e9fe435dfc6f5d5e8d119df567e2b321913434
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2026-07-20 14:16:37 +0800

    setup README

--- apps/tests/vite.config.ts
+++ apps/tests/vite.config.ts
@@ -1,6 +1,7 @@
 import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import { solidStart } from "../../packages/start/src/config";
+import { imagePlugin } from '@solidjs/image/vite';
 
 export default defineConfig({
   server: {
@@ -27,6 +28,46 @@ export default defineConfig({
     }),
     nitro({
       compressPublicAssets: true,
+    }),
+    imagePlugin({
+      local: {
+        sizes: [480, 600],
+        quality: 80,
+        publicPath: "public",
+      },
+      remote: {
+        transformURL(url) {
+          return {
+            src: {
+              source: `https://picsum.photos/seed/${url}/1200/900.webp`,
+              width: 1080,
+              height: 760,
+            },
+            variants: [
+              {
+                path: `https://picsum.photos/seed/${url}/800/600.jpg`,
+                width: 800,
+                type: "image/jpeg",
+              },
+              {
+                path: `https://picsum.photos/seed/${url}/400/300.jpg`,
+                width: 400,
+                type: "image/jpeg",
+              },
+              {
+                path: `https://picsum.photos/seed/${url}/800/600.png`,
+                width: 800,
+                type: "image/png",
+              },
+              {
+                path: `https://picsum.photos/seed/${url}/400/300.png`,
+                width: 400,
+                type: "image/png",
+              },
+            ],
+          };
+        },
+      },
     }),
   ],
 });
---
+++ packages/image/README.md
@@ -0,0 +1,25 @@
+# `@solidjs/image`
+
+## Install
+
+```bash
+npm i @solidjs/image
+```
+
+```bash
+pnpm add @solidjs/image
+```
+
+## Usage
+
+### Vite setup
+
+```ts
+import { imagePlugin } from '@solidjs/image/vite';
+
+export default defineConfig({
+  plugins: {
+    
+  }
+});
+```
