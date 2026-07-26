6021941823344b06e7f49b873f142abd8832edc1
Birk Skyum <birk.skyum@pm.me>
2026-07-26 14:27:05 +0200

    chore: oxc format and add rule

--- apps/tests/src/functions/use-is-server.ts
+++ apps/tests/src/functions/use-is-server.ts
@@ -5,7 +5,6 @@ import { keepAlive } from "~/utils/keep-alive-util";
 
 const serverSecret = "MyServerSuperSecretUniqueString2";
 
-
 export function serverFnWithIsServer() {
   keepAlive(serverSecret);
   return isServer;
