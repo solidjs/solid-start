5b4fbe7a47a4d9c5c4ba5a4f83f27cfa253e6210
huseeiin <husseinfadhil651@gmail.com>
2026-09-24 16:05:58 +0300

    fix

--- packages/start/src/config/index.ts
+++ packages/start/src/config/index.ts
@@ -245,8 +245,9 @@ export function solidStart(options?: SolidStartOptions): Array<PluginOption> {
       configEnvironment(name) {
         return {
           resolve: {
+            conditions: ["generic"],
             // remove when https://github.com/solidjs/vite-plugin-solid/pull/228 is released
-            externalConditions: ["solid", "node", "generic"],
+            externalConditions: ["solid", "node"],
           },
         };
       },
