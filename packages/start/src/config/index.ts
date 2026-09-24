026d3cd2dd027f0f0fceea6919dc0e5c69513ef1
huseeiin <husseinfadhil651@gmail.com>
2026-09-24 15:56:58 +0300

    fix cloudflare

--- packages/start/src/config/index.ts
+++ packages/start/src/config/index.ts
@@ -246,7 +246,7 @@ export function solidStart(options?: SolidStartOptions): Array<PluginOption> {
         return {
           resolve: {
             // remove when https://github.com/solidjs/vite-plugin-solid/pull/228 is released
-            externalConditions: ["solid", "node"],
+            externalConditions: ["solid", "node", "generic"],
           },
         };
       },
